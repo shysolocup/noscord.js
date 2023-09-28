8e7179dd8cc89567ce25d2958c1dcae7c8a7410a
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-28 14:14:04 -0500

    Update index.js

--- src/Client/index.js
+++ src/Client/index.js
@@ -1,5 +1,6 @@
 const cl = require('aepl');
 const { Soup } = require('stews');
+const fetch = import('fetch');
 
 const eventList = require('./Eventing/list.js');
 
@@ -8,13 +9,23 @@ exp.push("Client", cl.from(Client));
 
 
 class Client {
-    constructor() {
+    constructor(token) {
+        this.token = token;
+
+
+        // command handling
+        this.commands = new Soup({
+            msg: new Soup(Object),
+            slash: new Soup(Object)
+        });
+        
 
         /* event handling */
         this.events = new Soup(Object);
         eventList.forEach( (name) => {
             this.events.push( name, new this.Event() );
         });
+        
     }
 
     
@@ -27,6 +38,12 @@ class Client {
     // remove event listener
     off(event, func) {
         this.events.get(event).listeners.remove(func);
+    }
+
+
+    // login
+    login() {
+        this.shit.login(this.token);
     }
 }
 
