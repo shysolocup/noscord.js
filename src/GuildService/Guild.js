b31c2a3cbb65a5dcd0676ed859435bb19fce6bf9
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-28 14:35:15 -0500

    Create Guild.js

---
+++ src/GuildService/Guild.js
@@ -0,0 +1,12 @@
+const { Client } = require('../Client/index.js');
+const { Soup } = require('stews');
+
+
+Client.newC("Guild", class {
+    constructor() {
+        
+    }
+});
+
+
+module.exports = Guild;
