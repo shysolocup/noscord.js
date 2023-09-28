7afecb32d9c8eca390fe0410c3df071845e44f12
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-28 14:06:13 -0500

    Create Event.js

---
+++ src/Eventing/Event.js
@@ -0,0 +1,26 @@
+const { Client } = require('../index.js');
+const { Soup } = require('stews');
+
+
+Client.newClass("Event", class {
+    constructor() {
+        this.listeners = new Soup(Array);
+    }
+
+    
+    // listener
+    listen(func) {
+        this.listeners.push(func);
+    }
+
+    
+    // firing
+    fire(/**/) {
+        this.listeners.forEach( (lst) => {
+            lst(...Array.from(arguments));
+        });
+    }
+});
+
+
+module.exports = Event;
