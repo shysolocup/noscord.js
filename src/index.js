611d8830fb0f5421fa55a4ccf244fb46f68808ee
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-28 14:00:12 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 const cl = require('aepl');
 const { Soup } = require('stews');
 
+const events = require('./eventing/list.js');
+
 const exp = new Soup(Object);
 exp.push("Client", cl.from(Client));
 
 
 class Client {
     constructor() {
-        
+
+        /* event handling */
+        let E = this.Event;
+        this.events = new Soup(events);
     }
 }
 
