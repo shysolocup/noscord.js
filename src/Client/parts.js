c174c0d033690966d235701f40fb76f3e922a278
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-28 14:48:53 -0500

    Update parts.js

--- src/Client/parts.js
+++ src/Client/parts.js
@@ -4,4 +4,6 @@ module.exports = [
    require('../Services/ChannelService/funky_dir'),  // channel handling
    require('../Services/MessageService/funky_dir'),  // message handling
    require('../Services/StorageService/funky_dir'),  // storage handling
+   require('../Services/UserService/funky_dir'),  // user handling
+   require('../Services/CommandService/funky_dir'),  // cmd handling
 ];
