a47ddaf9d33c6cd10bbd62d11cc3469d2a12ad26
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-29 10:14:39 -0500

    Delete src/Client/parts.js

--- src/Client/parts.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-   require('../Services/EventService/funky_dir'),  // event handling
-   require('../Services/GuildService/funky_dir'),  // guild handling
-   require('../Services/ChannelService/funky_dir'),  // channel handling
-   require('../Services/MessageService/funky_dir'),  // message handling
-   require('../Services/StorageService/funky_dir'),  // storage handling
-   require('../Services/UserService/funky_dir'),  // user handling
-   require('../Services/CommandService/funky_dir'),  // cmd handling
-];
