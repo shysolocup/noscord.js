ba30e4b1913bb7cd5dac20ca085f0a2b23fcbccf
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-29 14:50:18 -0500

    Update list.js

--- src/Services/EventService/list.js
+++ src/Services/EventService/list.js
@@ -1,7 +1,7 @@
-module.exports = [ 
+module.exports = { 
     ["ready", "run", "login"],             // login
     ["messageCreate", "messageSent"]       // message sent
-];
+};
 
 
 const { Client } = require('../../Client');
