a612a5c436b4218f5a038bb1c25c6632ac0bd8f4
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-29 15:08:06 -0500

    Update text.js

--- src/Services/ChannelService/custard/text.js
+++ src/Services/ChannelService/custard/text.js
@@ -5,6 +5,7 @@ const { Soup } = require('stews');
 ChannelService.newF("text", async function() {
     return Soup.from(await this.parent.shit.channels
         .fetch()
-        .catch(e=>{}))
-        .filter( (id, channel) => { return channel.type == 0; });
+        .catch(e=>{})
+    )
+    .filter( (id, channel) => { return channel.type == 0; });
 });
