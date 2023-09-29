ac5421f9433a02b962c21214ad7c31cf234ff84b
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-29 14:25:39 -0500

    Create get.js

---
+++ src/Services/GuildService/custard/get.js
@@ -0,0 +1,12 @@
+const GuildService = require('../index.js');
+
+
+GuildService.newF("get", async function(id) {
+    if (!id) return null; 
+    
+    let raw = id; 
+    
+    let thing = await this.parent.shit.guilds.fetch(raw).catch(e=>{});
+    
+    return (!thing) ? null : new this.Guild(thing);
+});
