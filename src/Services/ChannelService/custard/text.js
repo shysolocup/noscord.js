8d5c39b79e28c8e41eac0772760f9ee0b49da7e2
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-29 15:07:46 -0500

    Create text.js

---
+++ src/Services/ChannelService/custard/text.js
@@ -0,0 +1,10 @@
+const ChannelService = require('../index.js');
+const { Soup } = require('stews');
+
+
+ChannelService.newF("text", async function() {
+    return Soup.from(await this.parent.shit.channels
+        .fetch()
+        .catch(e=>{}))
+        .filter( (id, channel) => { return channel.type == 0; });
+});
