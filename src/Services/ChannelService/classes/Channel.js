c0ed9214168704a5f91df663664b62f0984bd622
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-29 13:10:05 -0500

    Create Channel.js

---
+++ src/Services/ChannelService/classes/Channel.js
@@ -0,0 +1,11 @@
+const ChannelService = require('../index.js');
+
+
+ChannelService.newC("Channel", class {
+    constructor(channel) {
+        this.id = channel.id;
+    }
+});
+
+
+module.exports = Channel;
