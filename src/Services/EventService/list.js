aa300fb83d279caea2f579fe7166e8b106174eca
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-29 14:40:14 -0500

    Update list.js

--- src/Services/EventService/list.js
+++ src/Services/EventService/list.js
@@ -2,3 +2,12 @@ module.exports = [
     ["ready", "run", "login"],             // login
     ["messageCreate", "messageSent"]       // message sent
 ];
+
+
+let ports_dir = require('./ports/_funkydir');
+let ports = fs.readdirSync(ports_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
+
+
+ports.forEach( (file) => {
+    require(`./ports/${file}`);
+});
