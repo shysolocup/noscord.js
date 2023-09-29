60ba55df447350652411a71edcf7012536434bb9
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-29 15:08:46 -0500

    Create voice.js

---
+++ src/Services/ChannelService/custard/voice.js
@@ -0,0 +1,11 @@
+const ChannelService = require('../index.js');
+const { Soup } = require('stews');
+
+
+ChannelService.newF("voice", async function() {
+    return Soup.from(await this.parent.shit.channels
+        .fetch()
+        .catch(e=>{})
+    )
+    .filter( (id, channel) => { return channel.type == 2; });
+});
