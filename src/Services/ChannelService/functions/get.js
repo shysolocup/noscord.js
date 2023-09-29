eb32a1c71b43633c96815817d3fc1335525489b8
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-29 13:02:16 -0500

    Update get.js

--- src/Services/ChannelService/functions/get.js
+++ src/Services/ChannelService/functions/get.js
@@ -1,1 +1,6 @@
+const ChannelService = require('../index.js');
 
+
+ChannelService.newF("get", async function(id) {
+    if (!id) return null; let rawChannel = id; if (rawChannel.startsWith('<#') && rawChannel.endsWith('>')) {rawChannel = rawChannel.slice(2, -1); } rawChannel = rawChannel.split("").join(""); let channel = this.shit.channels.fetch(rawChannel).catch(e=>{}); return (!channel) ? null : channel;
+});
