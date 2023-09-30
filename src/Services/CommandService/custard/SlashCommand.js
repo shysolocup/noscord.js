076a075aa36a88ddd62a258492c9ba84a0aa60e5
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-30 10:17:55 -0500

    Create SlashCommand.js

---
+++ src/Services/CommandService/custard/SlashCommand.js
@@ -0,0 +1,12 @@
+const CommandService = require('../index.js');
+
+
+CommandService.newC("SlashCommand", class {
+	constructor(info, data) {
+		this.info = info;
+		this.data = data;
+	}
+});
+
+
+module.exports = Command;
