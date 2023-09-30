1f93908c2b1756748c913316333ad9d7b33b4daf
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-30 16:18:05 -0500

    Update get.js

--- src/Services/UserService/custard/get.js
+++ src/Services/UserService/custard/get.js
@@ -1,1 +1,21 @@
+const UserService = require('../index.js');
 
+
+UserService.newF("get", async function(id, guild=null) {
+    if (!id) return null; 
+    
+    let raw = id; 
+    
+    if (raw.startsWith('<@') && raw.endsWith('>')) {
+        raw = raw.slice(2, -1);
+    }
+    
+    raw = raw.split("").join(""); 
+    
+    let thing = await ( 
+		(guild) ? guild.users.fetch(raw).catch(e=>{}) : 
+		this.parent.shit.users.fetch(raw).catch(e=>{})
+	);
+    
+    return (!thing) ? null : thing;
+});
