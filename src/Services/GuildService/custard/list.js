e081b254b993e96f18cf1c757fe8b118c8965b61
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-30 12:40:11 -0500

    Update list.js

--- src/Services/GuildService/custard/list.js
+++ src/Services/GuildService/custard/list.js
@@ -3,5 +3,5 @@ const { Soup } = require('stews');
 
 
 GuildService.newF("list", async function() {
-    return Soup.from(await this.parent.shit.guilds.fetch().catch(e=>{}));
+    return Soup.from(await this.parent.shit.guilds.fetch().catch(e=>{})).pour();
 });
