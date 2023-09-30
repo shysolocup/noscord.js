3882fbc42b9e4e5f9de1218e16523b6e19f42ca8
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-30 11:11:36 -0500

    Update index.js

--- src/Client/index.js
+++ src/Client/index.js
@@ -1,5 +1,5 @@
 /*
-	:: WumpCli :: Alpha 1.0.0 | 09/29/23 ::
+	:: WumpCli :: Alpha 1.0.0 | 09/30/23 ::
 	https://github.com/paigeroid/wumpcli
 
 */
@@ -14,9 +14,13 @@
 
 const cl = require('aepl');
 const fs = require('fs');
+import('node-fetch');
 
 const { Soup } = require('stews');
-const { Client } = require('discord.js');
+const { Client, Routes, REST } = require('discord.js');
+
+const serv_dir = require('../Services/_funkydir');
+const cust_dir = require('./custard/_funkydir');
 
 const exp = new Soup(Object);
 
@@ -30,7 +34,6 @@ class WumpClient {
 
 
         /* service compiling */
-        let serv_dir = require('../Services/_funkydir');
         const Services = fs.readdirSync(serv_dir).filter( folder => (folder.endsWith("Service")) );
 
         Services.forEach( (folder) => {
@@ -40,7 +43,6 @@ class WumpClient {
 
 
         /* function compiling */
-        let cust_dir = require('./custard/_funkydir');
         let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
         
         cust.forEach( (file) => {
@@ -49,19 +51,79 @@ class WumpClient {
 
 
 
-         /* command handling */
-         this.commands = new Soup(Object);
+        /* command handling */
+        this.commands = new Soup(Object);
             
 
             
-         /* event handling */
-         this.events = new Soup(Object);
+        /* event handling */
+        this.events = new Soup(Object);
 		
-		 this.DefEvents = require('../Services/EventService/deflist.json');
+		this.DefEvents = require('../Services/EventService/deflist.json');
          
-		 this.DefEvents.forEach( (name) => {
-			 this.events.push( name, new this.Event() );
-		 });
+		this.DefEvents.forEach( (name) => {
+			this.events.push( name, new this.Event() );
+		});
+
+		
+
+		// registering slash commands
+		this.shit.on("ready", async (ctx) => {
+			if (this.commands.length <= 0) return
+			let token = this.token;
+
+
+			// registering commands
+			var jsonCommands = this.commands.values.map( (command) => {
+				let info = command.info.copy()
+				
+				// removing cooldown because it breaks it because it's stupid
+				if (info.has("cooldown")) info.delete("cooldown");
+	
+				return info.toJSON()
+				
+			});
+	
+			this.rest = new REST().setToken(token)
+			fetch('https://discordapp.com/api/oauth2/applications/@me', {
+				headers: {
+					authorization: `Bot ${token}`,
+				},
+			})
+	
+				.then(result => result.json())
+				.then( async (response) => {
+					const { id } = response;
+					
+					await this.rest.put(
+						Routes.applicationCommands(id),
+						{ body: jsonCommands }
+					);
+			})
+			.catch(console.error);
+
+
+
+			// registering commands
+			this.shit.on("interactionCreate", async (ctx) => {
+				if (ctx.isChatInputCommand() && this.commands.has(ctx.commandName)) {
+					let raw = this.commands.get(ctx.commandName);
+					
+					let cmd = Soup.from({
+						name: raw.info.get("name"),
+						description: raw.info.get("description"),
+						cooldown: raw.info.get("cooldown"),
+						options: raw.info.get("options"),
+						nsfw: raw.info.get("nsfw"),
+						data: raw.data
+		
+					}).copy().pour();
+					cmd.args = ctx.options.data;
+
+					return await raw.data(ctx, cmd);
+				}
+			});
+		});
     }
 }
 
