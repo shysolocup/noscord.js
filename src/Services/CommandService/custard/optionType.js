d81ee14c22bce6f31cee6b14474a520e60fb0140
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-30 10:17:39 -0500

    Create optionType.js

---
+++ src/Services/CommandService/custard/optionType.js
@@ -0,0 +1,30 @@
+const CommandService = require('../index.js');
+
+
+CommandService.newF("optionType", function (type) {
+	return (typeof type == "number")
+		? type
+	: ( ["sub_command", "sub_com", "sub"].includes(type.toLowerCase()) )
+		? 1
+	: ( ["sub_command_group", "sub_com_group", "sub_group"].includes(type.toLowerCase()) )
+		? 2
+	: ( ["string", "str"].includes(type.toLowerCase()) )
+		? 3
+	: ( ["integer", "int"].includes(type.toLowerCase()) )
+		? 4
+	: ( ["boolean", "bool"].includes(type.toLowerCase()) )
+		? 5
+	: ( ["user", "member"].includes(type.toLowerCase()) )
+		? 6
+	: (type.toLowerCase() == "channel")
+		? 7
+	: (type.toLowerCase() == "role")
+		? 8
+	: ( ["mentionable", "mention"].includes(type.toLowerCase())  )
+		? 9
+	: ( ["number", "num"].includes(type.toLowerCase()) )
+		? 10
+	: ( ["attachment", "file"].includes(type.toLowerCase()) )
+		? 5
+	: null;
+});
