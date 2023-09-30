8f6d03206c5a015595ce30170b6efc9e3f43ee7d
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-30 10:16:58 -0500

    Create create.js

---
+++ src/Services/CommandService/custard/create.js
@@ -0,0 +1,46 @@
+const CommandService = require('../index.js');
+const { Soup } = require('stews');
+
+
+CommandService.newF("create", function(info={name:null, description:null, options:null, cooldown:null, nsfw:false}, dataA, dataB=null) {
+	if (typeof info == "string") {
+		let thing = info;
+		info = { name: thing, description:null, options:null, nsfw:false };
+	}
+
+	if (info.options && info.options instanceof Array) {
+		var options = Soup.from(info.options)
+
+		options = options.map( (v) => {
+			v.type = this.parent.optionType(v.type);
+			if (v.desc && !v.description) v.description = v.desc;
+			return v;
+		})
+	}
+
+	if (info.desc && !info.description) info.description = info.desc
+
+	var data
+	if (typeof dataA == "string" && !info.description) {
+		info.description = dataA
+		data = dataB
+	}
+	else if (info.description) {
+		data = dataA
+	}
+	
+	
+	var [name, description] = [info.name, info.description];
+	
+	if (!name || typeof name != "string" || name.length <= 0) {
+		throw new CoolError("Slash Command Creation", "Invalid slash command name.\n\nPossible reasons:\n    • doesn't exist\n    • not a string\n    • blank string\n\nActual error stuff:");
+	}
+
+	if (!description || typeof description != "string" || description.length <= 0) {
+		throw new CoolError("Slash Command Creation", "Invalid slash command description.\n\nPossible reasons:\n    • not a string\n    • blank string\n\nActual error stuff:");
+	}
+
+	let cmd = new this.SlashCommand(info, data);
+	
+	this.parent.commands.push(cmd.info.name, cmd);
+});
