71a01f2573b46e0c4b23475d3817c65763c6929e
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-30 13:05:27 -0500

    Update get.js

--- src/Services/MessageService/custard/get.js
+++ src/Services/MessageService/custard/get.js
@@ -1,1 +1,10 @@
+const MessageService = require('../index.js');
 
+
+MessageService.newF("get", async function(id, channel) {
+    if (!id || !channel) return null;
+    
+    let thing = await channel.messages.fetch(id).catch(e=>{});
+    
+    return (!thing) ? null : thing;
+});
