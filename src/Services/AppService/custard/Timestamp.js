f54de0330e9ef629fbde5bfcaf63d5ee984ac9c9
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-30 17:04:25 -0500

    Create Timestamp.js

---
+++ src/Services/AppService/custard/Timestamp.js
@@ -0,0 +1,20 @@
+const AppService = require('../index.js');
+const { Soup } = require('stews');
+
+
+AppService.newC("Timestamp", class {
+	constructor(date) {
+		let dt = (date instanceof Date) ? date : (date) ? new Date(date) : new Date();
+
+		this.date = dt;
+		this.embed = dt.toISOString();
+		this.default = `<t:${Math.round(dt.getTime() / 1000)}>`;
+		this.shortTime = `<t:${Math.round(dt.getTime() / 1000)}:t>`;
+		this.longTime = `<t:${Math.round(dt.getTime() / 1000)}:T>`;
+		this.shortDate = `<t:${Math.round(dt.getTime() / 1000)}:d>`;
+		this.longDate = `<t:${Math.round(dt.getTime() / 1000)}:D>`;
+		this.shortDT = `<t:${Math.round(dt.getTime() / 1000)}:f>`;
+		this.longDT = `<t:${Math.round(dt.getTime() / 1000)}:F>`;
+		this.relative = `<t:${Math.round(dt.getTime() / 1000)}:R>`;
+	}
+});
