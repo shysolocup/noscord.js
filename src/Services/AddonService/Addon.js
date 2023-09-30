daa4ba22bffd173fa727fdb2cf69b8a082d6cfff
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-30 08:22:41 -0500

    Update Addon.js

--- src/Services/AddonService/Addon.js
+++ src/Services/AddonService/Addon.js
@@ -1,1 +1,11 @@
+const AddonService = require('./index.js');
 
+
+AddonService.newC("Addon", class {
+    constructor() {
+        return this.parent.parent.newC(...Array.from(arguments));
+    }
+});
+
+
+module.exports = Addon;
