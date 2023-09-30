16e42ed9dfc31af11713904ac5a31d86dec29b22
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-30 16:31:06 -0500

    Create list.js

---
+++ src/Services/UserService/custard/list.js
@@ -0,0 +1,20 @@
+const UserService = require('../index.js');
+const { Soup } = require('stews');
+
+
+UserService.newF("list", async function(guild=null) {
+    let users = new Soup( await ( (guild) ? guild.members : this.parent.shit.users)
+        .fetch()
+        .catch(e=>{})
+    );
+
+	
+	for (let i = 0; i < users.length; i++) {
+		users[i] = await this.get(users.keys[i]);
+	}
+	
+
+	return users
+		.filter( (id, user) => { return !user.bot })
+		.pour();
+});
