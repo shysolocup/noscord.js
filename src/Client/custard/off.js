f223452c23b4bbcdd0c811db53b2ed3a34776992
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-01 13:29:17 -0500

    Update off.js

--- src/Client/custard/off.js
+++ src/Client/custard/off.js
@@ -1,11 +1,11 @@
 const { Client } = require('../index.js');
 
 
-Client.newF("off", function(event, func) {
-    if (this.DefEvents.includes(event)) {
+Client.newF("off", function(event, func) { ( async () => {
+	if ((await this.DefEvents).includes(event)) {
 		this.shit.off(event, func);
 	}
-    else {
-        this.events.get(event).listeners.remove(func);
-    }
-});
+	else {
+    	this.events.get(event).listeners.remove(func);
+	}
+})()});
