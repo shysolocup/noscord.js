c9dbbd0d2f5f3f54fe8c6a7eee7e02d8e1de346a
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-02 13:23:53 -0500

    Update get.js

--- src/Services/RoleService/custard/get.js
+++ src/Services/RoleService/custard/get.js
@@ -1,1 +1,18 @@
+const RoleService = require('../index.js');
 
+
+RoleService.newF("get", async function(id) {
+    if (!id) return null; 
+    
+    let raw = id; 
+    
+    if (raw.startsWith('<@&') && raw.endsWith('>')) {
+        raw = raw.slice(3, -1);
+    }
+    
+    raw = raw.split("").join(""); 
+    
+    let thing = await this.parent.shit.roles.fetch(raw).catch(e=>{});
+    
+    return (!thing) ? null : thing;
+});
