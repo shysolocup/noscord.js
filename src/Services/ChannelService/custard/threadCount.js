9e1d5161cbc0a371b0847b089692defbf5d5ea8a
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-02 15:15:14 -0500

    Create threadCount.js

---
+++ src/Services/ChannelService/custard/threadCount.js
@@ -0,0 +1,12 @@
+const ChannelService = require('../index.js');
+const { Soup } = require('stews');
+
+
+ChannelService.newF("threadCount", async function(guild=null) {
+    return new Soup( await ( (guild) ? guild.channels : this.parent.shit.channels)
+        .fetch()
+        .catch(e=>{})
+    )
+		.filter( (id, channel) => { return channel.type == 11; })
+		.length;
+});
