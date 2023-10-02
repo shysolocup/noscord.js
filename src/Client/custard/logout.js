a71cae485f713d0e19b1b8dd964e8401adaf64c7
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-02 09:10:44 -0500

    Update logout.js

--- src/Client/custard/logout.js
+++ src/Client/custard/logout.js
@@ -1,6 +1,10 @@
 const { Client } = require('../index.js');
 
 
-Client.newF("logout", function() {
+function logout() {
     return this.shit.destroy();
-});
+}
+
+
+Client.newF("logout", logout);
+Client.newF("logout", destroy);
