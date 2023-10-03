83b03035d3fcb9708fb524c8337af21cfb338816
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-03 11:57:05 -0500

    Update on.js

--- src/Client/custard/on.js
+++ src/Client/custard/on.js
@@ -2,9 +2,12 @@ const { Client } = require('../index.js');
 
 
 Client.newF("on", function(event, func) { ( async () => {
+	let types = new this.TypeService;
+	
 	if ((await this.DefEvents).includes(event)) {
 		this.shit.on(event, func);
 	}
+		
 	else {
     	this.events.get(event).listen(func);
 	}
