db12c555a98060f6c41368f8c034faa282bf3e28
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-03 10:48:18 -0500

    Update index.js

--- src/Services/TypeService/index.js
+++ src/Services/TypeService/index.js
@@ -4,3 +4,10 @@ const fs = require('fs');
 Client.newC("TypeService");
 
 module.exports = TypeService;
+
+let type_dir = require('./types/_funkydir');
+let types = fs.readdirSync(type_dir).filter( folder => ( folder != "_funkydir" ) );
+
+types.forEach( (folder) => {
+    require(`./type/${folder}`);
+});
