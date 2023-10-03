2f464d7e64018285ec0ffda5d6be302b149aec13
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-03 10:46:16 -0500

    Update index.js

--- src/Services/ComponentService/index.js
+++ src/Services/ComponentService/index.js
@@ -1,10 +1,13 @@
 const { Client } = require('../../Client');
+const fs = require('fs');
 
 Client.newC("ComponentService");
 
 module.exports = ComponentService;
 
-require('./Button.js');
-require('./Selection.js');
-require('./Embed.js');
-require('./Row.js');
+let comp_dir = require('./components/_funkydir');
+let components = fs.readdirSync(comp_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
+
+components.forEach( (file) => {
+    require(`./components/${file}`);
+});
