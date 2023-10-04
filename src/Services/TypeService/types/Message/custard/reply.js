028e87c47ff907022c4a74c0066117c656454a88
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-04 12:07:31 -0500

    Update reply.js

--- src/Services/TypeService/types/Message/custard/reply.js
+++ src/Services/TypeService/types/Message/custard/reply.js
@@ -2,5 +2,29 @@ const Message = require('../index.js');
 
 
 Message.newF("reply", function() { (async (/**/) => {
-    const msg = this.raw;
+    let msg = this.raw;
+    let args = Array.from(this.arguments);
+    let [content, settings] = [ "", {} ];
+
+    if (args.length > 1) {
+        args.forEach( (arg) => {
+            if (typeof arg == "string") settings.content = arg;
+            if (arg instanceof Object && !(arg instanceof Array)) settings = Object.entries(settings).forEach( (a) => {
+                let [key, val] = a;
+                settings[key] = val;
+            });
+        });
+    }
+    
+    if (args[0] instanceof Array) {
+        args[0] = args[0].join(",");
+    }
+
+    if (args[0] instanceof Number) {
+        args[0] = parseFloat(args[0]);
+    }
+    
+    if (args[0] instanceof Object) {
+        
+    }
 })(); });
