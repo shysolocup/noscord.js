c710bfdf56111be77e61b867b011c8beea2b65e7
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-04 13:29:37 -0500

    Update index.js

--- src/Services/TypeService/types/MessageReply/index.js
+++ src/Services/TypeService/types/MessageReply/index.js
@@ -1,9 +1,13 @@
 const TypeService = require('../../index.js');
+const fs = require('fs');
 
-TypeService.newC("MessageReply", class {
-  constructor(ctx) {
-    
-  }
-});
+TypeService.newC("MessageReply");
 
 module.exports = MessageReply;
+
+let cust_dir = require('./custard/_funkydir');
+let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
+
+cust.forEach( (file) => {
+    require(`./custard/${file}`);
+});
