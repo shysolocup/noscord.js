b6ac8340c1aef5e508b8d3ed1532a51f8fb57832
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-04 14:38:11 -0500

    Create delete.js

---
+++ src/Services/TypeService/types/MessageReply/custard/delete.js
@@ -0,0 +1,14 @@
+const MessageReply = require('../index.js');
+
+
+MessageReply.newF("delete", function(after=0) {
+    const client = this.parent.parent;
+    const app = new client.AppService;
+    let msg = this.raw;
+
+    setTimeout( () => {
+        msg.delete().catch(e=>{});
+    }, app.parse(after)*1000 );
+
+    return this;
+});
