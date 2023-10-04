f6e4c956ac49d36f42995b60021b11f57b834d70
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-04 14:55:04 -0500

    Update index.js

--- src/Services/TypeService/types/ClientUser/index.js
+++ src/Services/TypeService/types/ClientUser/index.js
@@ -4,9 +4,11 @@ TypeService.newC("ClientUser");
 
 module.exports = ClientUser;
 
+/*
 let cust_dir = require('./custard/_funkydir');
 let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
 
 cust.forEach( (file) => {
     require(`./custard/${file}`);
 });
+*/
