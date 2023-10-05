a1304f8b9ee11e0cae873496c377105533ab9ca0
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-05 13:56:11 -0500

    Create get.js

---
+++ src/Services/VoiceService/custard/get.js
@@ -0,0 +1,7 @@
+const VoiceService = require('../index.js');
+const voice = require('@discordjs/voice');
+
+
+VoiceService.newF("get", function(channel) {
+    return voice.getVoiceConnection(channel.guild.id);
+})
