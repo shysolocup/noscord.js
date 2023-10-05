adbd5d8561759aaa0c1f88c1f3a0c400afdcd18b
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-05 08:04:34 -0500

    Update index.js

--- src/Services/TypeService/types/MessageEdit/index.js
+++ src/Services/TypeService/types/MessageEdit/index.js
@@ -1,72 +1,13 @@
 const TypeService = require('../../index.js');
-const { Soup } = require('stews');
+const fs = require('fs');
 
+TypeService.newC("MessageEdit");
 
-let msgEdit = TypeService.newC("MessageEdit");
+module.exports = MessageEdit;
 
+let cust_dir = require('./custard/_funkydir');
+let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
 
-msgEdit.newF("apply", async function(ctx) {
-    const client = this.parent.parent;
-    const guilds = new client.GuildService;
-    const channels = new client.ChannelService;
-    const users = new client.UserService;
-    const messages = new client.MessageService;
-
-
-    const guild = await guilds.get(ctx.guildId);
-    const channel = await channels.get(ctx.channelId);
-
-    
-    // content
-    this.content = ctx.content;
-    this.to = await messages.get(ctx.id, channel);
-
-
-    // ids
-    this.id = ctx.id;
-    this.channelId = ctx.channelId;
-    this.guildId = ctx.guildId;
-    this.webhookId = ctx.webhookId;
-    this.appId = ctx.applicationId;
-
-
-    // circle thingstuff
-    this.guild = guild
-    this.channel = channel;
-    this.author = (ctx.author) ? ctx.author : (ctx.user) ? ctx.user : undefined;
-    this.member = (ctx.member) ? ctx.member : await users.get(this.author.id, this.guild);
-
-
-    // other stuff
-    this.createdAt = ctx.createdTimestamp;
-    this.type = ctx.type;
-    this.system = ctx.system;
-
-
-    // even more stuff
-    this.pinned = ctx.pinned;
-    this.tts = ctx.tts;
-    this.nonce = ctx.nonce;
-
-
-    // lists
-    this.embeds = ctx.embeds;
-    this.components = ctx.components;
-    this.attachments = ctx.attachments;
-    this.stickers = ctx.stickers;
-    this.flags = ctx.flags;
-
-
-    // even even more more stuff stuff
-    this.position = ctx.position;
-    this.mentions = ctx.mentions;
-    this.reactions = ctx.reactions;
-    this.roleSubscriptionData = ctx.roleSubscriptionData;
-    this.editedAt = ctx.editedTimestamp;
-    this.activity = ctx.activity;
-    this.referece = ctx.reference;
-    this.interaction = ctx.interaction
+cust.forEach( (file) => {
+    require(`./custard/${file}`);
 });
-
-
-module.exports = msgEdit;
