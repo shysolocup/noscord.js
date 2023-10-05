e3347019de5de04ae1cbb5bf45b181b3d6308945
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-05 08:06:54 -0500

    Create edit.js

---
+++ src/Services/TypeService/types/MessageReply/custard/edit.js
@@ -0,0 +1,28 @@
+const MessageReply = require('../index.js');
+
+
+MessageReply.newF("edit", function(/**/) { return (async () => {
+    const client = this.parent.parent;
+    const app = new client.AppService;
+    const types = new client.TypeService;
+    
+    let msg = this.raw;
+    let args = Array.from(arguments);
+    let settings;
+
+    if (args[1] && args[1] instanceof Object) {
+        settings = args[1];
+        settings.content = args[0].toString();
+    }
+    else {
+        settings = args[0];
+    }
+
+    let edit = new types.MessageEdit();
+    await edit.apply( await msg.edit(settings) );
+
+    if (settings.deleteAfter) await edit.delete(settings.deleteAfter);
+
+    return edit;
+    
+})(); });
