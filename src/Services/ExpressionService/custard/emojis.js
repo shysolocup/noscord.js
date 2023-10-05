17602f80c40a68d35ced9d019e59389d3ce50609
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-05 14:40:09 -0500

    Update and rename emojiList.js to emojis.js

--- src/Services/ExpressionService/custard/emojis.js
+++ src/Services/ExpressionService/custard/emojis.js
@@ -2,6 +2,6 @@ const ExpressionService = require('../index.js');
 const { Soup } = require('stews');
 
 
-ExpressionService.newF("emojiList", async function(guild) {
+ExpressionService.newF("emojis", async function(guild) {
     return Soup.from(await guild.emojis.fetch()).pour();
 });
