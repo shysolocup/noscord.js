cd89af3d7b1f50af28aac932b7e61285456714e0
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-05 10:23:05 -0500

    Update index.js

--- src/Services/ErrorService/index.js
+++ src/Services/ErrorService/index.js
@@ -2,17 +2,6 @@ const { Client } = require('../../Client');
 const Soup = require('stews');
 const fs = require('fs');
 
-Client.newC("ErrorService", class {
-    constructor() {
-        this.errors = new Soup(Object);
-    }
-});
+Client.newC("ErrorService");
 
-module.exports = ErrorService;
-
-let cust_dir = require('./custard/_funkydir');
-let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
-
-cust.forEach( (file) => {
-    require(`./custard/${file}`);
-});
+require('./NosError.js);
