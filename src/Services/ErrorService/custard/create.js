441d68a8768937f78f10b52ebf2ca562a6e55b4a
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-05 10:19:53 -0500

    Create create.js

---
+++ src/Services/ErrorService/custard/create.js
@@ -0,0 +1,31 @@
+const ErrorService = require('../index.js');
+
+
+ErrorService.newC("NosError", class {
+    constructor(attachment, crash=true) {
+        const { stackTraceLimit } = Error;
+        Error.stackTraceLimit = 0;
+        
+        this.attachment = attachment;
+        this.crash = crash;
+        
+        Error.stackTraceLimit = stackTraceLimit;
+    }
+    
+    fire(name, message, crash) {
+        
+        // capturing the error
+        let error = new Error();
+        if (this.attachment) Error.captureStackTrace(error, this.attachment);
+        
+        // settings
+        error.name = name;
+        error.message = message;
+        
+        if (crash || this.crash) throw error
+        else console.log(error);
+    }
+});
+
+
+module.exports = NosError;
