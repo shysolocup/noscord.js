911cf4e1d7a7ab062cf8158930246ebc30b4aec6
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-05 14:40:24 -0500

    Update and rename stickerList.js to stickers.js

--- src/Services/ExpressionService/custard/stickers.js
+++ src/Services/ExpressionService/custard/stickers.js
@@ -2,6 +2,6 @@ const ExpressionService = require('../index.js');
 const { Soup } = require('stews');
 
 
-ExpressionService.newF("emojiList", async function(guild) {
+ExpressionService.newF("stickers", async function(guild) {
     return Soup.from(await guild.stickers.fetch()).pour();
 });
