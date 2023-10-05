7d619ef6a1c0a23723415a08e89847a99f24dfb1
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-05 13:32:26 -0500

    Create undeafen.js

---
+++ src/Services/VoiceService/custard/undeafen.js
@@ -0,0 +1,12 @@
+const VoiceService = require('../index.js');
+
+
+VoiceService.newF("undeafen", async function(user, guild) {
+    let channel = await this.find(user, guild);
+
+    if (channel.members.has(user.id)) {
+        let vcUser = channel.members.get(user.id);
+        vcUser.voice.setDeaf(false);
+        return vcUser;
+    }
+})
