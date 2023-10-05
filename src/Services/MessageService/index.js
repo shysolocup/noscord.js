bea2ac41180aaceea77d2922b24779f567260987
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-05 14:37:44 -0500

    Update index.js

--- src/Services/MessageService/index.js
+++ src/Services/MessageService/index.js
@@ -5,7 +5,6 @@ Client.newC("MessageService");
 
 module.exports = MessageService;
 
-
 let cust_dir = require('./custard/_funkydir');
 let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
 
