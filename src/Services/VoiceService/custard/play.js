669b38ef1f98cbf65cd3a76b4320e93bfa4c44db
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-05 13:58:55 -0500

    Update play.js

--- src/Services/VoiceService/custard/play.js
+++ src/Services/VoiceService/custard/play.js
@@ -1,7 +1,26 @@
 const VoiceService = require('../index.js');
 const voice = require('@discordjs/voice');
+const { Soup } = require('stews');
 
 
-VoiceService.newF("play", async function() {
+VoiceService.newF("play", async function(audioDir, guild=null) {
     
+    // gets channels the bot is in
+    let me = Soup.from(await this.me(guild));
+
+    
+    // stuff
+    let resource = voice.createAudioResource(audioDir);
+    let player = voice.createAudioPlayer();
+
+
+    // plays the audio
+    player.play(resource);
+
+
+    // adds channels included
+    me.forEach( (id, channel) => {
+        let connection = this.get(channel); 
+        connection.subscribe(channel);
+    })
 });
