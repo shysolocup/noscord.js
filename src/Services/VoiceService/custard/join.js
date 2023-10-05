06fdea46ecc1f1bf4f151a405f9db3c184a106c7
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-05 13:25:20 -0500

    Update join.js

--- src/Services/VoiceService/custard/join.js
+++ src/Services/VoiceService/custard/join.js
@@ -1,1 +1,11 @@
+const VoiceService = require('../index.js');
+const voice = require('@discordjs/voice');
 
+
+VoiceService.newF("join", async function(channel) {
+    voice.joinVoiceChannel({
+        channelId: channel.id,
+        guildId: channel.guild.id,
+        adapterCreator: channel.guild.voiceAdapterCreator
+    });
+})
