3874a00a16601bb99a749131d4cc13d02cece16e
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-06 13:16:44 -0500

    Update index.js

--- src/Services/TypeService/types/Attachment/index.js
+++ src/Services/TypeService/types/Attachment/index.js
@@ -2,11 +2,15 @@ const TypeService = require('../../index.js');
 const { AttachmentBuilder } = require('discord.js');
 
 TypeService.newC("Attachment", class {
-    constructor(canvas) {
-        this.png = new AttachmentBuilder( canvas.toBuffer("image/png"), settings ),
-		this.jpeg = new AttachmentBuilder( canvas.toBuffer("image/jpeg"), settings ),
-		this.url = canvas.toDataURL(),
-		this.raw = canvas
+    constructor(canvas, settings={}) {
+        this.png = new AttachmentBuilder( canvas.toBuffer("image/png"), settings );
+		this.jpeg = new AttachmentBuilder( canvas.toBuffer("image/jpeg"), settings );
+
+		this.url = canvas.toDataURL();
+		this.raw = canvas;
+
+        this.width = canvas.width;
+        this.height = canvas.height;
     }
 });
 
