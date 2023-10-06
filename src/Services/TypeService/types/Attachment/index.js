b83e98b96fc078e9afdb086cd7e694566f5f4e5f
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-06 14:02:37 -0500

    Update index.js

--- src/Services/TypeService/types/Attachment/index.js
+++ src/Services/TypeService/types/Attachment/index.js
@@ -11,7 +11,16 @@ TypeService.newC("Attachment", class {
 
         this.width = canvas.width;
         this.height = canvas.height;
+
+        this.settings = settings;
     }
 });
 
 module.exports = Attachment;
+
+let cust_dir = require('./custard/_funkydir');
+let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
+
+cust.forEach( (file) => {
+    require(`./custard/${file}`);
+});
