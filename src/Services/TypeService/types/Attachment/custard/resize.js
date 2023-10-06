e3af121210736ae51b82cb4547e1bd63f9b0e564
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-06 14:03:08 -0500

    Create resize.js

---
+++ src/Services/TypeService/types/Attachment/custard/resize.js
@@ -0,0 +1,19 @@
+const Attachment = require('../index.js');
+const Canvas = require('@napi-rs/canvas');
+
+
+Attachment.newF("resize", async function(width, height, settings={}) {
+    let img = this.png.attachment;
+
+	let image = await Canvas.loadImage(img);
+	
+	width = (width) ? width : image.width;
+	height = (height) ? height : image.height;
+	
+	let canvas = Canvas.createCanvas(width, height);
+	let context = canvas.getContext('2d');
+	
+	context.drawImage(image, 0, 0, width, height);
+	
+	return new Attachment(canvas, settings);
+});
