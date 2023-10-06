d5b2cda92b43ae72f2e02f39ee09fa4e51ec977b
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-06 11:25:52 -0500

    Update onSubmit.js

--- src/Client/custard/onSubmit.js
+++ src/Client/custard/onSubmit.js
@@ -4,7 +4,7 @@ const { Client } = require('../index.js');
 Client.newF("onSubmit", function(a, b) { ( async () => {
 	if (b && b instanceof Function) {
 		this.on("selectSubmit", async (ctx) => {
-			if (ctx.id == a) await b(ctx);
+			if (ctx.customId == a) await b(ctx);
 		});
 	}
 	else {
