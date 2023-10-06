4a9f9be6b26b33be86a4890c3334bd3d36409839
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-06 13:54:38 -0500

    Update create.js

--- src/Services/AttachmentService/custard/create.js
+++ src/Services/AttachmentService/custard/create.js
@@ -2,14 +2,14 @@ const AttachmentService = require('../index.js');
 const Canvas = require('@napi-rs/canvas');
 
 
-AttachmentService.newF("create", async function(imgUrl, settings={}) {
+AttachmentService.newF("create", async function(img, settings={}) {
 	const client = this.parent;
     let types = new client.TypeService;
 
     if (img.constructor instanceof types.Attachment) img = img.png.attachment;
     else if (img.constructor.name == "AttachmentBuilder") img = img.attachment;
 	
-	let image = await Canvas.loadImage(imgUrl);
+	let image = await Canvas.loadImage(img);
 	
 	let width = (settings.width) ? settings.width : image.width;
 	let height = (settings.height) ? settings.height : image.height;
