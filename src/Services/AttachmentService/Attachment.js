6a1ce804d0cde45c642d6c336ca3a41e421319e1
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-06 11:34:24 -0500

    Create Attachment.js

---
+++ src/Services/AttachmentService/Attachment.js
@@ -0,0 +1,23 @@
+const { AttachmentBuilder } = require('discord.js');
+const AttachmentService = require('./index.js');
+const Canvas = require('@napi-rs/canvas');
+
+
+AttachmentService.newC("Attachment", class {
+    constructor(settings) {
+        let image = await Canvas.loadImage(iconURL);
+        
+        let width = (settings.width) ? settings.width : image.width;
+        let height = (settings.height) ? settings.height : image.height;
+        
+        let canvas = Canvas.createCanvas(width, height);
+		let context = canvas.getContext('2d');
+
+        context.drawImage(image, 0, 0, width, height);
+
+        return new AttachmentBuilder(canvas.toBuffer("image/png"), settings);
+    }
+});
+
+
+module.exports = Attachment;
