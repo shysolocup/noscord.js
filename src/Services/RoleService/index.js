1d8b927a235dc51e25b826ac002252aeb7ccaf7a
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-06 11:41:09 -0500

    Update index.js

--- src/Services/RoleService/index.js
+++ src/Services/RoleService/index.js
@@ -5,7 +5,6 @@ Client.newC("RoleService");
 
 module.exports = RoleService;
 
-
 let cust_dir = require('./custard/_funkydir');
 let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
 
