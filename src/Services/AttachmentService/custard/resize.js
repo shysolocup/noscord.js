f804dbd0fabb7fc719666af91d1fd3d17fe8baa5
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-06 13:26:56 -0500

    Create resize.js

---
+++ src/Services/AttachmentService/custard/resize.js
@@ -0,0 +1,26 @@
+const { AttachmentBuilder } = require('discord.js');
+const AttachmentService = require('../index.js');
+const Canvas = require('@napi-rs/canvas');
+
+
+AttachmentService.newF("resize", async function(img, width, height, settings={}) {
+    const client = this.parent;
+    let types = new client.TypeService;
+
+    console.log(Object.getOwnPropertyDescriptors(img));
+
+    if (img.name) img = img.png.attachment;
+    else if (img.constructor.name == "AttachmentBuilder") img = img.attachment;
+
+	let image = await Canvas.loadImage(img);
+	
+	width = (width) ? width : image.width;
+	height = (height) ? height : image.height;
+	
+	let canvas = Canvas.createCanvas(width, height);
+	let context = canvas.getContext('2d');
+	
+	context.drawImage(image, 0, 0, width, height);
+	
+	return new types.Attachment(canvas, settings);
+});
