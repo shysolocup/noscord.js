c0e539333528e49dcf2bbf5343791e2ba8e0952d
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-06 13:58:38 -0500

    Update resize.js

--- src/Services/AttachmentService/custard/resize.js
+++ src/Services/AttachmentService/custard/resize.js
@@ -6,7 +6,7 @@ AttachmentService.newF("resize", async function(img, width, height, settings={})
     const client = this.parent;
     let types = new client.TypeService;
 
-    if (img.constructor instanceof types.Attachment) img = img.png.attachment;
+    if (img instanceof types.Attachment) img = img.png.attachment;
     else if (img.constructor.name == "AttachmentBuilder") img = img.attachment;
 
 	let image = await Canvas.loadImage(img);
