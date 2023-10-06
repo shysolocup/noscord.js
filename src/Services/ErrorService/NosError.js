670f454d09d861b06fb5a7600eaeea5a2de6ad07
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-06 14:34:30 -0500

    Update NosError.js

--- src/Services/ErrorService/NosError.js
+++ src/Services/ErrorService/NosError.js
@@ -2,10 +2,14 @@ const ErrorService = require('./index.js');
 
 
 ErrorService.newC("NosError", class {
-    constructor(attachment) {
+    constructor(attachment, name, message) {
         const { stackTraceLimit } = Error;
         Error.stackTraceLimit = 0;
+        
         this.attachment = attachment;
+        this.name = name;
+        this.message = message;
+        
         Error.stackTraceLimit = stackTraceLimit;
     }
     
@@ -16,8 +20,8 @@ ErrorService.newC("NosError", class {
         if (this.attachment) Error.captureStackTrace(error, this.attachment);
         
         // settings
-        error.name = name;
-        error.message = message;
+        error.name = (name) ? name : this.name;
+        error.message = (message) ? message : this.message;
 
         // if it should crash
         if (crash) throw error;
