87a7c13b7f46d0dcaa5444145e7740982fdc9d25
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-06 11:25:35 -0500

    fixed id

--- src/Client/custard/onPress.js
+++ src/Client/custard/onPress.js
@@ -4,7 +4,7 @@ const { Client } = require('../index.js');
 Client.newF("onPress", function(a, b) { ( async () => {
 	if (b && b instanceof Function) {
 		this.on("buttonPress", async (ctx) => {
-			if (ctx.id == a) await b(ctx);
+			if (ctx.customId == a) await b(ctx);
 		});
 	}
 	else {
