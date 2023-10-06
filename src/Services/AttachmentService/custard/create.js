bb1672645e4b89d1d44bab4615b305edd0ff9104
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-06 11:38:00 -0500

    Update and rename src/Services/AttachmentService/Attachment.js to src/Services/AttachmentService/custard/create.js

---
+++ src/Services/AttachmentService/custard/create.js
@@ -0,0 +1,18 @@
+const { AttachmentBuilder } = require('discord.js');
+const AttachmentService = require('./index.js');
+const Canvas = require('@napi-rs/canvas');
+
+
+AttachmentService.newF("create", async function(imgUrl, settings) {
+	let image = await Canvas.loadImage(imgUrl);
+	
+	let width = (settings.width) ? settings.width : image.width;
+	let height = (settings.height) ? settings.height : image.height;
+	
+	let canvas = Canvas.createCanvas(width, height);
+	let context = canvas.getContext('2d');
+	
+	context.drawImage(image, 0, 0, width, height);
+	
+	return new AttachmentBuilder( canvas.toBuffer("image/png"), settings );
+});
