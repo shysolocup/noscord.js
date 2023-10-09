5dc09589d7e95a161ccfda36c7eeff900df65935
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-09 12:35:47 -0500

    Update _deflist.js

--- src/Services/EventService/_deflist.js
+++ src/Services/EventService/_deflist.js
@@ -2,7 +2,7 @@ import('node-fetch');
 
 
 // fetching events
-let events = async () => {
+let events = (async () => {
 	events = await fetch("https://github.com/discordjs/discord.js/blob/main/packages/discord.js/src/util/Events.js");	
 	let result = await events.json();
 	return Object.values(eval(result.payload.blob.rawLines.join("\n")));
