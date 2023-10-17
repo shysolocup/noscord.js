206527066b2a0be12e5fff35d7fcbb272f960650
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-17 10:30:55 -0500

    Update on.js

--- src/Client/custard/on.js
+++ src/Client/custard/on.js
@@ -3,5 +3,5 @@ const { Client } = require('../index.js');
 
 Client.newF("on", function(event, func) { ( async () => {
 	let handler = this._handler;
-	handler.on(event, func);
+	return await handler.on(event, func);
 })()});
