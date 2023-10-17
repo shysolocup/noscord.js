4c9566a9fdb6c417f5df85ff7d7db8466f21e655
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-17 10:06:40 -0500

    Update _eventHandle.js

--- src/Client/custard/_eventHandle.js
+++ src/Client/custard/_eventHandle.js
@@ -5,5 +5,5 @@ const { Soup } = require('stews');
 Client.newC("_eventHandle", function(/**/) {
     const client = this;
     const types = client.types;
-    const base = client.base;
+    const base = client._base;
 });
