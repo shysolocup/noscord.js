f6edde9e115eca61c38a6abdbe1dd5f1ea7bc807
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-17 11:45:25 -0500

    Update init.js

--- src/Services/EventService/handiddle/custard/init.js
+++ src/Services/EventService/handiddle/custard/init.js
@@ -3,10 +3,14 @@ const EventHandler = require('../index.js');
 
 EventHandler.newF("init", (name, func, glue=null) => {
     let types = this.types;
+	let client = this.client;
+	
+	let event = new client.Event();
+	client.events.push(name, event);
 	
 	if (glue && (await this.defaults).includes(glue)) {
-		return this.shit.on(glue, async function (/**/) {
-            return await func(...Array.from(arguments));
+		this._base.on(glue, async function (/**/) {
+            await event.fire( await func(...Array.from(arguments)) );
         });
     }
 });
