81a26b106f09ddfe17a1f98cccdb33363934b796
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-17 10:16:38 -0500

    Update list.js

--- src/Services/GuildService/custard/list.js
+++ src/Services/GuildService/custard/list.js
@@ -3,7 +3,7 @@ const { Soup } = require('stews');
 
 
 GuildService.newF("list", async function() {
-    let guilds = Soup.from(await this.parent.shit.guilds.fetch().catch(e=>{}));
+    let guilds = Soup.from(await this.parent._base.guilds.fetch().catch(e=>{}));
 	
 	for (let i = 0; i < guilds.length; i++) {
 		guilds[i] = await this.get(guilds.keys[i]);
