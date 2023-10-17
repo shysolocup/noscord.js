0c40eb2536878a03f156672d756de395e33e11e4
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-17 14:15:38 -0500

    Update apply.js

--- src/Services/TypeService/types/GuildMemberAction/custard/apply.js
+++ src/Services/TypeService/types/GuildMemberAction/custard/apply.js
@@ -1,12 +1,29 @@
 const GuildMemberAction = require('../index.js');
 
 
-GuildMemberAction.newF("apply", async function(ctx) {
+GuildMemberAction.newF("apply", async function(ctx, actionType) {
     const client = this.parent.parent;
     const guilds = new client.GuildService;
     const channels = new client.ChannelService;
     const users = new client.UserService;
 
     
-    
+    this.actionType = actionType;
+    this.id = ctx.id;
+    this.bot = ctx.bot;
+    this.me = (ctx.id == client.id);
+
+
+    if (actionType == 1) { // kick
+        const logs = await ctx.guild.fetchAuditLogs({
+            limit: 1,
+            type: 'MEMBER_KICK',
+        });
+
+        const log = fetchedLogs.entries.first();
+
+        if (log) {
+            this.moderator = await users.get(log.executor.id, ctx.guild);
+        }
+    }
 });
