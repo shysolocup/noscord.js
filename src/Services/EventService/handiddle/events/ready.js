3a46b6766b7a3832b3aad0719e04a5182b03f4f4
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-17 12:36:16 -0500

    Create ready.js

---
+++ src/Services/EventService/handiddle/events/ready.js
@@ -0,0 +1,15 @@
+module.exports = (handler) => { handler.init(
+
+    
+    name = "ready",  // name of the event
+
+    
+    func = async function (/**/) { // formatting for types and stuff
+        return Array.from(arguments);
+    }, 
+
+    
+    glue = "ready" // what discord.js event it's tied to
+
+    
+)}
