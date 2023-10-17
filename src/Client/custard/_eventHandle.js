f1853fa9ff260088bcbd7badfb7c07d8a088af69
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-17 10:06:05 -0500

    Create _eventHandle.js

---
+++ src/Client/custard/_eventHandle.js
@@ -0,0 +1,9 @@
+const { Client } = require('../index.js');
+const { Soup } = require('stews');
+
+
+Client.newC("_eventHandle", function(/**/) {
+    const client = this;
+    const types = client.types;
+    const base = client.base;
+});
