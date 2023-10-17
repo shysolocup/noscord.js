dcd718f03f91d880493771c66940e14ea8bf9fba
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-17 11:47:26 -0500

    Update messageCreate.js

--- src/Services/EventService/handiddle/events/messageCreate.js
+++ src/Services/EventService/handiddle/events/messageCreate.js
@@ -1,12 +1,20 @@
-module.exports = (handler) => {
+module.exports = (handler) => { handler.init(
+
     
-    handler.init("messageCreate", async (/**/) => {
+    name = "messageCreate",  // name of the event
+
+        
+    func = async (/**/) => { // formatting for types and stuff
         let types = this.types;
         let msg = new types.Message;
         
         await msg.apply(...Array.from(arguments));
     
         return msg;
-    });
+    }, 
+
     
-}
+    glue = "messageCreate" // what discord.js event it's tied to
+
+    
+)}
