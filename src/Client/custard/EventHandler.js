9b9ca4ff2b0620753b623db18684a8f8c356233f
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-17 10:27:40 -0500

    Update and rename _eventHandle.js to EventHandler.js

---
+++ src/Client/custard/EventHandler.js
@@ -0,0 +1,16 @@
+const { Client } = require('../index.js');
+const { Soup } = require('stews');
+
+
+Client.newC("EventHandler", class {
+    constructor() {
+        this.client = this.parent;
+        this.types = this.client.types;
+        this._base = this.client._base;
+    }
+
+
+    init(name, type) {
+        
+    }
+});
--- src/Client/custard/_eventHandle.js
+++
@@ -1,9 +0,0 @@
-const { Client } = require('../index.js');
-const { Soup } = require('stews');
-
-
-Client.newC("_eventHandle", function(/**/) {
-    const client = this;
-    const types = client.types;
-    const base = client._base;
-});
