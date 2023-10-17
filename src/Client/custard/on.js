2ab33d59546bc636cfc057d27d7b233cb72c7ec5
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-17 12:51:44 -0500

    Update on.js

--- src/Client/custard/on.js
+++ src/Client/custard/on.js
@@ -2,12 +2,25 @@ const { Client } = require('../index.js');
 
 
 Client.newF("on", function(event, func) { ( async () => {
-	if (this.events.has(event)) {
-		this.events.get(event).listen(func);
-	}
+	if (event instanceof Array) {
+		event.forEach( (e) => {
+			if (this.events.has(e)) {
+				this.events.get(e).listen(func);
+			}
 
+			else {
+				this._base.on(e, func);
+			}
+		})
+	}
 	else {
-		this._base.on(event, func);
+		if (this.events.has(event)) {
+			this.events.get(event).listen(func);
+		}
+
+		else {
+			this._base.on(event, func);
+		}
 	}
 
 	return func;
