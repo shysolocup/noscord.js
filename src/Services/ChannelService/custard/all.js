4350b5f478b46a35c01f3de6f5cfe3fd9d157499
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-17 10:11:51 -0500

    Update all.js

--- src/Services/ChannelService/custard/all.js
+++ src/Services/ChannelService/custard/all.js
@@ -3,5 +3,5 @@ const { Soup } = require('stews');
 
 
 ChannelService.newF("all", async function() {
-    return Soup.from(await this.parent.shit.channels.fetch().catch(e=>{}));
+    return Soup.from(await this.parent._base.channels.fetch().catch(e=>{}));
 });
