64e535d8c7d483db476f41ef0bb16a0a89ada78f
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-17 20:37:21 -0500

    Create buttonPress.js

---
+++ src/Services/EventService/glue/events/buttonPress.js
@@ -0,0 +1,18 @@
+module.exports = (handler) => { handler.init(
+
+    
+    name = "buttonPress",  // names of the event
+
+    
+    func = function (/**/) { // formatting for types and stuff
+        return Array.from(arguments);
+    }, 
+
+    
+    glue = "interactionCreate", // what discord.js event it's tied to
+
+    
+    term = function(ctx) { // specific action that triggers the event
+        return ctx.isButton()
+    }
+)}
