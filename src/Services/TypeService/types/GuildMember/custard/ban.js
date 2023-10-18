b4604aab7199cfe4ca4798b5875f3ac6c55c7781
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-18 11:49:17 -0500

    Update ban.js

--- src/Services/TypeService/types/GuildMember/custard/ban.js
+++ src/Services/TypeService/types/GuildMember/custard/ban.js
@@ -1,19 +1,21 @@
 const GuildMember = require('../index.js');
 
 
-GuildMember.newF("ban", function(settings={ /* reason and duration */ }) {
+GuildMember.newF("ban", async function(settings={ /* reason and duration */ }) {
     const client = this.parent.parent;
     client.import("app", "errors", "users");
     
     let e = errors.create("Member Banning");
 
-    let user = this.raw;
+    let user = await users.get(this.id, this.guild);
     if (!user.bannable) e.fire(null, "User cannot be banned");
-    let thing = user.ban(settings).catch(e=>{});
+    else {
+        let thing = user.kick(settings).catch(e=>{});
 
-    if (settings.duration) setTimeout( () => {
-         user.guild.members.unban(user.id);
-    }, app.parseMs(settings.duration) );
+        if (settings.duration) setTimeout( () => {
+             user.guild.members.unban(user.id);
+        }, app.parseMs(settings.duration) );
 
-    return thing;
+        return thing;
+    }
 });
