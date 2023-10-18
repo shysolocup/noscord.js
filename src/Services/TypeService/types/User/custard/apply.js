c5d166cc3f76e0e4677a91a7d7c81f6ff37b145b
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-18 13:25:20 -0500

    Update apply.js

--- src/Services/TypeService/types/User/custard/apply.js
+++ src/Services/TypeService/types/User/custard/apply.js
@@ -5,20 +5,45 @@ User.newF("apply", async function(ctx) {
     const client = this.parent.parent;
     client.import("guilds", "messages", "channels", "users");
 
-    // ids
-    this.id = ctx.id;
+    // names
     this.username = ctx.username;
     this.tag = (ctx.tag.endsWith("#0")) ? ctx.username : ctx.tag;
+    this.discriminator = ctx.discriminator;
+    this.globalName = ctx.globalName;
+    
+    
+    // ids
+    this.id = ctx.id;
+    this.flags = ctx.flags;
+    this.bot = ctx.bot;
+
+
+    // accessories
+    this.avatar = await users.avatar(ctx);
+    this.avatarUrl = users.avatarUrl(ctx);
+    this.decor = await users.decor(ctx);
+    this.decorUrl = users.decorUrl(ctx)
+    this.banner = await users.banner(ctx);
+    this.bannerUrl = await users.bannerUrl(ctx);
+
+    
+    this.accent = {
+        base10: ctx.acccentColor,
+        hex: ctx.hexAccentColor
+    };
+    
+
+
+    // presence
     this.presence = ctx.presence;
     this.status = ctx.presence.status;
     this.activity = ctx.presence.activity;
-    this.bot = false;
+    this.system = ctx.system;
+    this.partial = ctx.partial;
 
 
-    // avatar
-    this.avatar = await users.avatar(ctx);
-    this.avatarUrl = await users.avatarUrl(ctx);
-
+    this.dms = ctx.dmChannel
+    
     
     this.raw = ctx;
 });
