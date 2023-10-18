8e7b93e3bb7dd34ee52a78a39aacca8610ae1332
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-18 12:28:44 -0500

    Update apply.js

--- src/Services/TypeService/types/GuildMemberAction/custard/apply.js
+++ src/Services/TypeService/types/GuildMemberAction/custard/apply.js
@@ -9,6 +9,7 @@ GuildMemberAction.newF("apply", async function(ctx, actionType) {
     this.isKick = (actionType == 1);
     this.isBan = (actionType == 2);
     this.isUnban = (actionType == 3);
+    this.isTimeout = (actionType == 4);
 
     this.victim = await users.get(ctx.id, ctx.guild);
 
@@ -22,6 +23,39 @@ GuildMemberAction.newF("apply", async function(ctx, actionType) {
 
         if (log) {
             this.moderator = await users.get(log.executor.id, ctx.guild);
+            this.reason = log.reason;
+            this.log = log;
+        }
+    }
+
+    else if (this.isBan) {
+        const logs = await ctx.guild.fetchAuditLogs({
+            limit: 1,
+            type: AuditLogEvent.MemberBanAdd,
+        });
+
+        const log = logs.entries.first();
+
+        if (log) {
+            this.moderator = await users.get(log.executor.id, ctx.guild);
+            this.reason = log.reason;
+            this.log = log;
+        }
+    }
+
+
+    else if (this.isUnban) {
+        const logs = await ctx.guild.fetchAuditLogs({
+            limit: 1,
+            type: AuditLogEvent.MemberBanRemove,
+        });
+
+        const log = logs.entries.first();
+
+        if (log) {
+            this.moderator = await users.get(log.executor.id, ctx.guild);
+            this.reason = log.reason;
+            this.log = log;
         }
     }
 });
