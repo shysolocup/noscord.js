3823b27ed477f5c0ca1f3aac5b6ba89009a3a75c
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-18 12:47:57 -0500

    Update apply.js

--- src/Services/TypeService/types/GuildMember/custard/apply.js
+++ src/Services/TypeService/types/GuildMember/custard/apply.js
@@ -3,18 +3,16 @@ const GuildMember = require('../index.js');
 
 GuildMember.newF("apply", async function(ctx) {
     const client = this.parent.parent;
-    client.import("guilds", "messages", "channels", "users");
+    client.import("guilds", "messages", "channels", "users", "app");
 
     // ids
     this.id = ctx.id;
     this.username = ctx.username;
     this.tag = (ctx.tag.endsWith("#0")) ? ctx.username : ctx.tag;
-    this.bannable = ctx.bannable;
-    this.nickname = ctx.nickname;
-    this.presence = ctx.presence;
-    this.status = ctx.presence.status;
-    this.activity = ctx.presence.activity;
-    this.bot = false;
+    this.user = await users.get(ctx.id);
+    this.nickname = ctx.displayName;
+    this.flags = ctx.flags;
+    this.bot = this.users.bot;
 
 
     // guild
@@ -25,6 +23,43 @@ GuildMember.newF("apply", async function(ctx) {
     // avatar
     this.avatar = await users.avatar(ctx);
     this.avatarUrl = await users.avatarUrl(ctx);
+
+
+    // presence
+    this.presence = ctx.presence;
+    this.status = ctx.presence.status;
+    this.activity = ctx.presence.activity;
+
+
+    // colors
+    this.color = {
+        base10: ctx.displayColor,
+        hex: ctx.displayHexColor
+    };
+
+    
+    // dms
+    this.dms = ctx.dmChannel;
+
+
+    // times
+    this.times = {
+        joined: new Timestamp(ctx.joinedAt),
+        disabled: new Timestamp(ctx.communicationDisabledUntil),
+        premium: new Timestamp(ctx.premiumSince),
+    };
+
+
+    // moderation stuff
+    this.bannable = ctx.bannable;
+    this.kickable = ctx.kickable;
+    this.manageable = ctx.manageable;
+    this.moderatable = ctx.moderatable;
+
+
+    // other stuff idk
+    this.pending = ctx.pending;
+    this.voice = ctx.voice;
 
     
     this.raw = ctx;
