e8ea0edf4dc6c11a218187c91b713f41be612ccd
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-18 13:39:20 -0500

    Update index.js

--- src/Services/TypeService/types/Message/index.js
+++ src/Services/TypeService/types/Message/index.js
@@ -1,13 +1,21 @@
 const TypeService = require('../../index.js');
 const fs = require('fs');
 
-TypeService.newC("Message");
+TypeService.newC("Guild", class {
+    [Symbol.toPrimitive](hint) {
+        if (hint === "string") {
+            return this.url
+        }
+    }
+});
 
-module.exports = Message;
+module.exports = Guild;
 
+/*
 let cust_dir = require('./custard/_funkydir');
 let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
 
 cust.forEach( (file) => {
     require(`./custard/${file}`);
 });
+*/
