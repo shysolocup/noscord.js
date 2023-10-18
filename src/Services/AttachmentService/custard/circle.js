698924a6e931de345d847733529b005275dece74
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-18 01:31:37 -0500

    Update circle.js

--- src/Services/AttachmentService/custard/circle.js
+++ src/Services/AttachmentService/custard/circle.js
@@ -2,7 +2,7 @@ const AttachmentService = require('../index.js');
 const Canvas = require('@napi-rs/canvas');
 
 
-AttachmentService.newF("circle", async function(img, width, height, settings={}) {
+AttachmentService.newF("circle", async function(img, radius, settings={}) {
     const client = this.parent;
     let types = new client.TypeService;
 
@@ -11,15 +11,15 @@ AttachmentService.newF("circle", async function(img, width, height, settings={})
 
 	let image = await Canvas.loadImage(img);
 	
-	width = (width) ? width : image.width;
-	height = (height) ? height : image.height;
+	let width = (settings.width) ? settings.width : image.width;
+	let height = (settings.height) ? settings.height : image.height;
 	
 	let canvas = Canvas.createCanvas(width, height);
 	let context = canvas.getContext('2d');
 
     context.strokeRect(0, 0, width, height);
 	context.beginPath();
-	context.arc(width/2, height/2, 100, 0, Math.PI * 2, true);
+	context.arc(width/2, height/2, width/2, 0, Math.PI * 2, true);
 	context.closePath();
 	context.clip();
 	
