646964577ab7a75343e46d5e281e11098058b758
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-17 20:19:11 -0500

    Update commandRan.js

--- src/Services/EventService/glue/events/commandRan.js
+++ src/Services/EventService/glue/events/commandRan.js
@@ -4,8 +4,24 @@ module.exports = (handler) => { handler.init(
     name = "commandRan",  // names of the event
 
     
-    func = function () { // formatting for types and stuff
-        return Array.from(arguments);
+    func = function (ctx) { // formatting for types and stuff
+        let client = this.parent.parent;
+        let raw = client.commands.get(ctx.commandName);
+        ctx.author = ctx.user;
+        
+        let cmd = Soup.from({
+
+            name: raw.info.get("name"),
+            description: raw.info.get("description"),
+            cooldown: raw.info.get("cooldown"),
+            options: raw.info.get("options"),
+            nsfw: raw.info.get("nsfw"),
+            data: raw.data
+
+        }).copy().pour();
+        cmd.args = ctx.options.data;
+        
+        return [ctx, cmd];
     }, 
 
     
