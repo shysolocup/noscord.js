2acc20023d07e15568be86f99f39301c82d860bb
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-18 13:09:05 -0500

    Update edit.js

--- src/Services/TypeService/types/Message/custard/edit.js
+++ src/Services/TypeService/types/Message/custard/edit.js
@@ -8,7 +8,7 @@ Message.newF("edit", function(/**/) { return (async () => {
     
     let msg = this.raw;
     let args = Array.from(arguments);
-    let settings;
+    let settings = {};
 
     if (args[1] && args[1] instanceof Object) {
         settings = args[1];
@@ -16,6 +16,25 @@ Message.newF("edit", function(/**/) { return (async () => {
     }
     else {
         settings = args[0];
+    }
+
+    if (settings.embeds) {
+        settings.embeds.map( (embed) => {
+            let img = embed.image.url;
+            let tn = embed.thumbnail.url;
+            
+            if (img instanceof types.Attachment || img.constructor.name == "AttachmentBuilder") {
+                embed.image.url = `attachment://${img.name}`;
+                (settings.files) ? settings.files.push(img) : (()=>{ settings.files = []; settings.files.push(img); })();
+            }
+            
+            if (tn instanceof types.Attachment || img.constructor.name == "AttachmentBuilder") {
+                embed.thumbnail.url = `attachment://${tn.name}`;
+                (settings.files) ? settings.files.push(tn) : (()=>{ settings.files = []; settings.files.push(tn); })();
+            }
+
+            return embed;
+        });
     }
 
     let edit = new types.MessageEdit();
