8459e3d4ba208d37b3094c3b0c266a7047b25cdb
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-18 11:48:09 -0500

    Create kick.js

---
+++ src/Services/TypeService/types/GuildMember/custard/kick.js
@@ -0,0 +1,17 @@
+const GuildMember = require('../index.js');
+
+
+GuildMember.newF("kick", async function(settings={ /* reason */ }) {
+    const client = this.parent.parent;
+    client.import("app", "errors", "users");
+    
+    let e = errors.create("Member Kicking");
+
+    let user = await users.get(this.id, this.guild);
+    if (!user.bannable) e.fire(null, "User cannot be kicked");
+    else {
+        let thing = user.kick(settings).catch(e=>{});
+
+        return thing;
+    }
+});
