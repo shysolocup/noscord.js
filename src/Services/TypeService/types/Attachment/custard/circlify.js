de3e0d1f31701adbd83affd87e6cc1698cad6e6d
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-18 01:38:33 -0500

    Create circlify.js

---
+++ src/Services/TypeService/types/Attachment/custard/circlify.js
@@ -0,0 +1,31 @@
+const Attachment = require('../index.js');
+const Canvas = require('@napi-rs/canvas');
+
+
+Attachment.newF("circlify", async function(settings={}) {
+    let img = this.png.attachment;
+    
+    const client = this.parent;
+    let types = new client.TypeService;
+
+    if (img instanceof types.Attachment) img = img.png.attachment;
+    else if (img.constructor.name == "AttachmentBuilder") img = img.attachment;
+
+	let image = await Canvas.loadImage(img);
+	
+	let width = (settings.width) ? settings.width : image.width;
+	let height = (settings.height) ? settings.height : image.height;
+	
+	let canvas = Canvas.createCanvas(width, height);
+	let context = canvas.getContext('2d');
+
+    context.strokeRect(0, 0, width, height);
+	context.beginPath();
+	context.arc(width/2, height/2, width/2, 0, Math.PI * 2, true);
+	context.closePath();
+	context.clip();
+	
+	context.drawImage(image, 0, 0, width, height);
+	
+	return new types.Attachment(canvas, settings);
+});
