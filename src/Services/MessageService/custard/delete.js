71815d12e99ea2aeb4d12a9ec15b9429d175da49
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-19 10:23:53 -0500

    Update delete.js

--- src/Services/MessageService/custard/delete.js
+++ src/Services/MessageService/custard/delete.js
@@ -5,11 +5,11 @@ MessageService.newF("delete", async function(msg, settings={}) {
     const client = this.parent;
     client.import("app");
 
-    if (settings.after) {
-        setTimeout( () => {
-            msg.delete().catch(e=>{});
-        }, app.parseMs(settings.after) );
-    }
+    if (!settings.after) settings.after = 0;
+
+    setTimeout( () => {
+        msg.delete().catch(e=>{});
+    }, app.parseMs(settings.after) );
 
     return this;
 });
