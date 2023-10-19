766d0dcf0dbbd8febbeaa9ea012f91d1d531aeae
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-19 11:59:21 -0500

    Create channelEdit.js

---
+++ src/Services/EventService/glue/events/channelEdit.js
@@ -0,0 +1,18 @@
+module.exports = (handler) => { handler.init(
+
+    
+    name = ["channelEdit", "channelUpdate"],  // names of the event
+
+    
+    func = async function (ctx) { // formatting for types and stuff
+        let types = this.types;
+        let action = new types.GuildChannelAction;
+        
+        await action.apply(...Array.from(arguments));
+        
+        return [action, action.moderator, action.guild];
+    }, 
+
+    
+    glue = "channelDelete", // what discord.js event it's tied to
+)}
