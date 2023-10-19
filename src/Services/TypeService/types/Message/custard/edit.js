cfb6d7dfdcbd926b471f03d2bd76808825c9e110
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-19 10:00:51 -0500

    Update edit.js

--- src/Services/TypeService/types/Message/custard/edit.js
+++ src/Services/TypeService/types/Message/custard/edit.js
@@ -1,27 +1,10 @@
 const Message = require('../index.js');
 
 
-Message.newF("edit", function(/**/) { return (async () => {
+Message.newF("edit", function(...args) {
     const client = this.parent.parent;
-    client.import("app", "types", "components");
-    
+    client.import("messages");
     let msg = this.raw;
-    let args = Array.from(arguments);
-    let settings = {};
-
-    if (args[1] && args[1] instanceof Object) {
-        settings = args[1];
-        settings.content = args[0].toString();
-    }
-    else {
-        settings = args[0];
-    }
-
-    let edit = new types.MessageEdit();
-    await edit.apply( await msg.edit(settings) );
-
-    if (settings.deleteAfter) await edit.delete(settings.deleteAfter);
-
-    return edit;
     
-})(); });
+    return messages.edit(msg, ...args);
+});
