23333f3561dd887c725735f96ec90d5b1c930256
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-19 09:53:41 -0500

    Create reply.js

---
+++ src/Services/MessageService/custard/reply.js
@@ -0,0 +1,27 @@
+const MessageService = require('../index.js');
+
+
+MessageService.newF("reply", async function(...args) {
+    const client = this.parent;
+    client.import("app", "types", "components");
+    
+    let msg = args.shift();
+    let settings = {};
+
+    if (args[1] && args[1] instanceof Object) {
+        settings = args[1];
+        settings.content = args[0].toString();
+    }
+    else {
+        settings = args[0];
+    }
+
+    settings.files = [];
+
+    let reply = new types.Message();
+    await reply.apply( await msg.reply(settings) );
+
+    if (settings.deleteAfter) await reply.delete(settings.deleteAfter);
+
+    return reply;
+});
