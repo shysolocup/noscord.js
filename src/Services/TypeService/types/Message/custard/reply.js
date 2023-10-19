c984c81c6c928ad12794d26ff4cf075536b1b911
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-19 09:55:02 -0500

    Update reply.js

--- src/Services/TypeService/types/Message/custard/reply.js
+++ src/Services/TypeService/types/Message/custard/reply.js
@@ -1,29 +1,10 @@
 const Message = require('../index.js');
 
 
-Message.newF("reply", function(/**/) { return (async () => {
+Message.newF("reply", function(...args) {
     const client = this.parent.parent;
-    client.import("app", "types", "components");
-    
+    client.import("messages");
     let msg = this.raw;
-    let args = Array.from(arguments);
-    let settings = {};
-
-    if (args[1] && args[1] instanceof Object) {
-        settings = args[1];
-        settings.content = args[0].toString();
-    }
-    else {
-        settings = args[0];
-    }
-
-    settings.files = [];
-
-    let reply = new types.Message();
-    await reply.apply( await msg.reply(settings) );
-
-    if (settings.deleteAfter) await reply.delete(settings.deleteAfter);
-
-    return reply;
     
-})(); });
+    return messages.reply(msg, ...args);
+});
