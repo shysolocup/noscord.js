1643701ee6717945177de49c2577ac81745a505e
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-19 11:58:07 -0500

    Update apply.js

--- src/Services/TypeService/types/GuildChannelAction/custard/apply.js
+++ src/Services/TypeService/types/GuildChannelAction/custard/apply.js
@@ -1,1 +1,63 @@
+const GuildChannelAction = require('../index.js');
 
+
+GuildChannelAction.newF("apply", async function(ctx, actionType) {
+    const client = this.parent.parent;
+    client.import("messages", "channels", "guilds", "app");
+
+    // ids
+    this.id = ctx.id;
+    this.name = ctx.name;
+    this.nsfw = ctx.nsfw;
+    this.type = ctx.type;
+    this.viewable = ctx.viewable;
+    this.category = ctx.parent;
+    this.categoryId = ctx.parentId;
+    this.guildId = ctx.guildId;
+    this.url = `https://discord.com/channels/${ctx.guildId}/${ctx.id}`;
+    this.mention = `<#${ctx.id}>`;
+    this.topic = ctx.topic;
+
+
+    // stuff
+    this.guild = await guilds.get(ctx.guildId);
+    this.flags = ctx.flags;
+    this.partial = ctx.partial;
+    this.permissionOverwrites = ctx.permissionOverwrites;
+    this.permissionsLocked = ctx.permissionsLocked;
+    this.position = ctx.position;
+    this.rateLimitPerUser = ctx.rateLimitPerUser;
+    this.rawPosition = ctx.rawPosition;
+    this.threads = await channels.threads(ctx);
+
+
+    // messages
+    this.messages = ctx.messages;
+    this.msgList = await messages.list(ctx);
+    this.lastMsg = await messages.get(ctx.lastMessageId);
+    this.lastMsgId = ctx.lastMessageId;
+
+
+    this.isDelete = (actionType == 1);
+    this.isEdit = (actinType == 2);
+
+
+    // times
+    let deleted = this.isDelete;
+    let edited = this.isEdit;
+    
+    this.timestamps = {
+        created: new Timestamp(ctx.createdAt),
+        lastPin: new Timestamp(ctx.lastPinAt),
+        deleted: (deleted) ? new Timestamp() : undefined,
+        edited: (edited) ? new Timestamp() : undefined
+    }
+    
+
+
+    // booleans
+    this.deleteable = ctx.deleteable;
+
+
+    this.raw = ctx;
+});
