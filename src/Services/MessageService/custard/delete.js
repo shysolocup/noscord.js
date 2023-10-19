7f9756f02f77c89a9e00147589d3ec66d10f60c4
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-19 10:16:08 -0500

    Update delete.js

--- src/Services/MessageService/custard/delete.js
+++ src/Services/MessageService/custard/delete.js
@@ -4,7 +4,6 @@ const MessageService = require('../index.js');
 MessageService.newF("delete", async function(msg, after=0) {
     const client = this.parent;
     client.import("app");
-    msg = await this.get(msg.id, msg.channel);
 
     setTimeout( () => {
         msg.delete().catch(e=>{});
