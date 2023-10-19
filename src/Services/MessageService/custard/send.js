2efa8074128b9eceea57f84727cd02a1623123b8
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-19 09:52:42 -0500

    Create send.js

---
+++ src/Services/MessageService/custard/send.js
@@ -0,0 +1,27 @@
+const MessageService = require('../index.js');
+
+
+MessageService.newF("send", async function(...args) {
+    const client = this.parent;
+    client.import("app", "types", "components");
+    
+    let channel = args.shift();
+    let settings = {};
+
+    if (args[1] && args[1] instanceof Object) {
+        settings = args[1];
+        settings.content = args[0].toString();
+    }
+    else {
+        settings = args[0];
+    }
+
+    settings.files = [];
+
+    let msg = new types.Message();
+    await msg.apply( await channel.send(settings) );
+
+    if (settings.deleteAfter) await msg.delete(settings.deleteAfter);
+
+    return msg;
+});
