17bbe399480fd271e48fabe2500af3825338cf6e
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-19 07:59:19 -0500

    Update index.js

--- src/Services/TypeService/types/GuildMemberAction/index.js
+++ src/Services/TypeService/types/GuildMemberAction/index.js
@@ -1,7 +1,13 @@
 const TypeService = require('../../index.js');
 const fs = require('fs');
 
-TypeService.newC("GuildMemberAction");
+TypeService.newC("GuildMemberAction", class {
+    [Symbol.toPrimitive](hint) {
+        if (hint === "string") {
+            return `<@${this.victim.id}>`;
+        }
+    }
+});
 
 module.exports = GuildMemberAction;
 
