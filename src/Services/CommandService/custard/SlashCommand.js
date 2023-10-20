cd354d136491616a6154fb1d56942c29838b0bf6
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-20 12:39:55 -0500

    Update SlashCommand.js

--- src/Services/CommandService/custard/SlashCommand.js
+++ src/Services/CommandService/custard/SlashCommand.js
@@ -4,8 +4,30 @@ const { Soup } = require('stews');
 
 CommandService.newC("SlashCommand", class {
 	constructor(info, data) {
-		this.info = new Soup(info);
+		const client = this.parent.parent;
+
+		info = new Soup(info);
+
+		this.name = info.name;
+		this.description = info.description;
+		this.options = info.options;
+		this.nsfw = info.nsfw;
+			
 		this.data = data;
+
+		// adds it to the list
+		client.commands.push(this.name, this);
+	}
+
+	get info() {
+		let stuff = this;
+		
+		return new Soup({ 
+			name: stuff.name, 
+			description: stuff.description, 
+			options: stuff.options, 
+			nsfw: stuff.nsfw 
+		});
 	}
 });
 
