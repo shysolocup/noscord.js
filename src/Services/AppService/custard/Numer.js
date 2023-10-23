1e32cb0f669500d1fd325fe2c9ce7209a8b933a2
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-23 13:15:15 -0500

    Update Numer.js

--- src/Services/AppService/custard/Numer.js
+++ src/Services/AppService/custard/Numer.js
@@ -3,14 +3,23 @@ const { Soup } = require('stews');
 
 
 AppService.newC("Numer", class {
-	constructor(formatted, raw, currency, round) {
+	constructor(formatted, raw, currency, roundTo) {
 		this.value = formatted;
         this.raw = raw;
         this.currency = currency;
-        this.round = round;
-
-		this.even = (raw % 2 == 0);
+        this.roundTo = roundTo;
+        
+        this.even = (raw % 2 == 0);
         this.odd = (raw % 2 == 1);
+        
+        
+        return new Proxy(this, {
+            get(target, prop) {
+                if (target.value[prop]) return target.value[prop].bind(target.value);
+                else if (target.raw[prop]) return target.raw[prop].bind(target.raw);
+                else return target[prop];
+            } 
+        });
 	}
 
     [Symbol.toPrimitive](hint) {
