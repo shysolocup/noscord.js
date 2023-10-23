20e6a3f9f7e75fcbd200e92a6721f9a35f148e5b
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-23 04:49:56 -0500

    Update spread.js

--- src/Client/custard/spread.js
+++ src/Client/custard/spread.js
@@ -2,9 +2,12 @@ const { Client } = require('../index.js');
 const fs = require('fs');
 
 
-Client.newF("spread", function(path, ignore=["index.js", "index.ts"], action=(path, file) => { require(`../../../../../${path}/${file}`)(this); }) {
+Client.newF("spread", function(path, data=null, ignore=["index.js", "index.ts"], action=(path, file, spreads) => { require(`../../../../../${path}/${file}`)(...spreads); }) {
     this.on("ready", () => {   
+		if (!data) data = [this];
+		
         let files = fs.readdirSync(`${path}`).filter(file => ((file.endsWith('.js') || file.endsWith('.ts')) && !ignore.includes(file) ));
-	    files.forEach( (file) => { action.bind(this)(path, file) } );
+		
+	    files.forEach( (file) => { action(path, file, data) } );
     })
 });
