d0648628e1da5243a2b48c87b2644ff07d216ba5
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-22 23:54:17 -0500

    Update Timestamp.js

--- src/Services/AppService/custard/Timestamp.js
+++ src/Services/AppService/custard/Timestamp.js
@@ -20,6 +20,7 @@ AppService.newC("Timestamp", class {
 	}
 
 	latency(msg) {
+		console.log(msg.constructor.name);
 		if (msg.constructor.name == "ChatInputCommandInteraction") return this.date - msg.createdTimestamp
 		else if (msg.constructor.name == "Message") return this.date - msg.timestamps.created.time;
 		else if (msg.constructor.name == "MessageEdit") return this.date - msg.timestampes.edited.time;
