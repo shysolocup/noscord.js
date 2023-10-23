f677474d1f37af2fa746b8918d1a2e5da2f04271
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-23 13:16:04 -0500

    Update numer.js

--- src/Services/AppService/custard/numer.js
+++ src/Services/AppService/custard/numer.js
@@ -3,19 +3,19 @@ const AppService = require('../index.js');
 
 AppService.newF("numer", function (number, settings={}) {
     if (!(number instanceof Number)) number = parseFloat(number);
-    if (!settings.round) settings.round = 0;
+    if (!settings.roundTo) settings.roundTo = 0;
     if (!settings.currency) settings.currency = "";
 
     
     let formatter = new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'USD',
-        maximumFractionDigits: settings.round
+        maximumFractionDigits: settings.roundTo
     });
 
     
     let formatted = formatter.format(number).replace("$", settings.currency);
 
     
-    return new this.Numer(formatted, number, settings.currency, settings.round);
+    return new this.Numer(formatted, number, settings.currency, settings.roundTo);
 });
