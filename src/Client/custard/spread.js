125354dfd368ecc2fc125bb3a36949eb4415747e
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-23 04:35:28 -0500

    Create spread.js

---
+++ src/Client/custard/spread.js
@@ -0,0 +1,10 @@
+const { Client } = require('../index.js');
+const fs = require('fs');
+
+
+Client.newF("spread", function(path, ignore=["index.js", "index.ts"], action=(path, file) => { require(`../../${path}/${file}`); }) {
+    this.on("ready", () => {   
+        let files = fs.readdirSync(`${path}`).filter(file => ((file.endsWith('.js') || file.endsWith('.ts')) && !ignore.includes(file) ));
+	    files.forEach( (file) => { action(path, file) } );
+    }
+});
