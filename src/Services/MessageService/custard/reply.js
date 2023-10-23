85a543023bbd92d272251c4104f0e891f6c537f0
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-23 02:20:17 -0500

    Update reply.js

--- src/Services/MessageService/custard/reply.js
+++ src/Services/MessageService/custard/reply.js
@@ -19,7 +19,6 @@ MessageService.newF("reply", async function(...args) {
     settings.files = [];
 
     let reply = new types.Message();
-    console.log(await msg.reply(settings));
     await reply.apply( await msg.reply(settings).catch(e=>{}) );
 
     if (settings.deleteAfter) await reply.delete(settings.deleteAfter);
