54ce3584fe76967eea5f82fe348904568fd431ca
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-22 23:58:31 -0500

    Update latency.js

--- src/Services/AppService/custard/latency.js
+++ src/Services/AppService/custard/latency.js
@@ -3,5 +3,8 @@ const AppService = require('../index.js');
 
 AppService.newF("latency", function(msg, timestamp=null) {
 	if (!timestamp) timestamp = new this.Timestamp();
-    return timestamp.date - msg.createdTimestamp
+	if (msg.constructor.name == "ChatInputCommandInteraction") return timestamp.date - msg.createdTimestamp
+	else if (msg.constructor.name == "Message") return timestamp.date - msg.timestamps.created.date;
+	else if (msg.constructor.name == "MessageEdit") return timestamp.date - msg.timestampes.edited.date;
+	else return timestamp.date - msg.createdAt;
 });
