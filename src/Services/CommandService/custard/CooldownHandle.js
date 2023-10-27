eaed0645dc7f6724bfd132bcd08ad235e5bd57b1
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-27 09:32:58 -0500

    Update CooldownHandle.js

--- src/Services/CommandService/custard/CooldownHandle.js
+++ src/Services/CommandService/custard/CooldownHandle.js
@@ -4,7 +4,7 @@ const { Soup } = require('stews');
 
 CommandService.newC("CooldownHandle", class {
 	constructor(cooldown) {
-        const client = this.parent.parent;
+    	const client = this.parent.parent;
         client.import("app");
         
         this.data = new Soup(Object);
@@ -12,8 +12,16 @@ CommandService.newC("CooldownHandle", class {
 
 		this.time = app.parse(cooldown);
 		this.timeMs = this.time*1000;
+	}
 
-        this.userHandle = this.CooldownUserHandle;
+	get timestamp() {
+		return new Timestamp( new Date( Date.now() + this.timeMs ) );
+	}
+
+	cool(user) {
+		let handle = new this.CooldownUserHandle(user);
+		this.data.push(user.id, handle);
+		return handle;
 	}
 });
 
