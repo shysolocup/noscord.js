33de65422e56005cf2ad3d7d06b08e0300c23072
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-27 19:32:24 -0500

    Update timeout.js

--- src/Services/TypeService/types/GuildMember/custard/timeout.js
+++ src/Services/TypeService/types/GuildMember/custard/timeout.js
@@ -3,12 +3,12 @@ const GuildMember = require('../index.js');
 
 GuildMember.newF("timeout", async function(time) {
     const client = this.parent.parent;
-    client.import("app", "errors", "users");
+    client.import("util", "errors", "users");
     
     let e = errors.create("Member Timeout");
 
     let user = await users.get(this.id, this.guild);
-    let thing = user.timeout(app.parseMs(time)).catch(e=>{});
+    let thing = user.timeout(util.parseMs(time)).catch(e=>{});
 
     return thing;
 });
