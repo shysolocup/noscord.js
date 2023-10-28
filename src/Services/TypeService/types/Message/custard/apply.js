2af8949b22488411717e779787fea0c48d76f501
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-27 19:33:46 -0500

    Update apply.js

--- src/Services/TypeService/types/Message/custard/apply.js
+++ src/Services/TypeService/types/Message/custard/apply.js
@@ -3,7 +3,7 @@ const Message = require('../index.js');
 
 Message.newF("apply", async function(ctx) {
     const client = this.parent.parent;
-    client.import("guilds", "messages", "channels", "users", "app");
+    client.import("guilds", "messages", "channels", "users", "util");
 
     
     // content
