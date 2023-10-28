09fe263c7ed39698acf9f6049578fcc94a762574
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-27 19:36:17 -0500

    Update banner.js

--- src/Services/UserService/custard/banner.js
+++ src/Services/UserService/custard/banner.js
@@ -3,11 +3,11 @@ const UserService = require('../index.js');
 
 UserService.newF("banner", async function(user, settings={}) {
     const client = this.parent;
-    client.import({ att: "attachments" }, "app")
+    client.import({ att: "attachments" }, "util")
 
     let bannerUrl = user.bannerURL(settings.dynamic);
 
-    if (!settings.name) settings.name = app.genCode(9);
+    if (!settings.name) settings.name = util.genCode(9);
 
     let stuff = att.create(bannerUrl, settings);
     stuff.url = bannerUrl;
