659007f26deab2a1ec276f33b4c20b0e05825d5b
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-27 19:16:58 -0500

    Update and rename random.js to random.js

--- src/Services/AppService/custard/random.js
+++
@@ -1,5 +0,0 @@
-const AppService = require('../index.js');
-const { random } = require('stews');
-
-
-AppService.newP("random", random);
---
+++ src/Services/UtilService/custard/random.js
@@ -0,0 +1,5 @@
+const UtilService = require('../index.js');
+const { random } = require('stews');
+
+
+UtilService.newP("random", random);
