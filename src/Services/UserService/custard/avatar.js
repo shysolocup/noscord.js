7bb49e48e09e12d46813f04bf96b1ec56e8115a6
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-27 19:35:42 -0500

    Update avatar.js

--- src/Services/UserService/custard/avatar.js
+++ src/Services/UserService/custard/avatar.js
@@ -3,11 +3,11 @@ const UserService = require('../index.js');
 
 UserService.newF("avatar", async function(user, settings={}) {
     const client = this.parent;
-    client.import({ att: "attachments" }, "app");
+    client.import({ att: "attachments" }, "util");
 
     let avatarUrl = user.displayAvatarURL(settings.dynamic);
 
-    if (!settings.name) settings.name = app.genCode(9);
+    if (!settings.name) settings.name = util.genCode(9);
 
     let stuff = att.create(avatarUrl, settings);
     stuff.url = avatarUrl;
