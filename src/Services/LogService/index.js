c523c2385deb8d5e45c9dc74352edd38a919321f
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-30 11:53:45 -0500

    Create index.js

---
+++ src/Services/LogService/index.js
@@ -0,0 +1,13 @@
+const { Client } = require('../../Client');
+const fs = require('fs');
+
+Client.newC("LogService");
+
+module.exports = LogService;
+
+let cust_dir = require('./custard/_funkydir');
+let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
+
+cust.forEach( (file) => {
+    require(`./custard/${file}`);
+});
