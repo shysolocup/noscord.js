5d78855c27f72307567a39f4b370c190881847df
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-30 13:21:09 -0500

    Update snowflake.js

--- src/Services/UtilService/custard/snowflake.js
+++ src/Services/UtilService/custard/snowflake.js
@@ -2,5 +2,5 @@ const UtilService = require('../index.js');
 
 
 UtilService.newF("snowflake", function(date=new Date()) {
-    return return ((BigInt(date.valueOf()) - BigInt(1420070400000)) << BigInt(22)).toString();
+    return ((BigInt(date.valueOf()) - BigInt(1420070400000)) << BigInt(22)).toString();
 });
