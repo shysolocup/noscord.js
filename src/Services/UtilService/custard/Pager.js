81e2d7ce7402ef5362c2f95e22cf7083a1dad406
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-30 08:04:32 -0500

    Create Pager.js

---
+++ src/Services/UtilService/custard/Pager.js
@@ -0,0 +1,48 @@
+const UtilService = require('../index.js');
+const { Soup } = require('stews');
+
+
+UtilService.newC("Pager", class {
+	constructor(per, object) {
+        var obj = Soup.from(object);
+
+        var paged = new Soup({
+            pages: 1,
+            length: 0,
+            contents: new Soup([ (obj.isPair()) ? Soup.from(Object) : Soup.from(Array) ]),
+
+            page(page) {
+                return this.contents[page-1];
+            }
+        });
+
+
+        var page = 0;
+        var count = 0;
+
+
+        for (let i = 0; i < obj.length; i++) {
+                if (!paged.contents.get(page+1) && count >= per) { 
+                    page += 1; 
+                    paged.pages += 1; 
+
+                    paged.contents.push( (obj.isPair()) ? Soup.from(Object) : Soup.from(Array) ); 
+                    
+                    count = 0;
+                }
+
+
+                if (obj.isPair()) paged.contents[page].push( ...obj.entries[i] );
+                else paged.contents[page].push( obj[i] );
+
+                paged.length += 1;
+                count += 1;
+       
+        }
+
+        return paged;
+    }
+});
+
+
+module.exports = Pager;
