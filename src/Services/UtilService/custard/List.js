db16e508760db063f34d1a1cff1f2348c073887c
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-30 09:35:34 -0500

    Update List.js

--- src/Services/UtilService/custard/List.js
+++ src/Services/UtilService/custard/List.js
@@ -6,54 +6,16 @@ UtilService.newC("List", class {
 	constructor(per, object, start=1) {
         var obj = Soup.from(object);
 
-        var paged = new Soup({
-            pages: 1,
-            length: 0,
+        this.pages: 1;
+    	this.length: 0;
 			
-			page: {
-				num: start,
-				content: null
-			},
-			
-            contents: new Soup([ (obj.isPair()) ? Soup.from(Object) : Soup.from(Array) ]),
+		this.page: {
+			num: start,
+			content: null
+		};
 
-            get(page) {
-                return this.contents[page-1];
-            },
-
-			to(page) {
-				this.page.num = page;
-				this.page.content = this.get(page);
-
-				return this.page;
-			},
-
-			next(by=1) {
-				let { num, content } = this.page;
-
-				if (num+by > this.pages) {
-					this.page.num = 1;
-					this.page.content = this.get(1);
-				}
-				else {
-					this.page.num = num+by;
-					this.page.content = this.get(num+by);
-				}
-			},
-
-			prev(by=1) {
-				let { num, content } = this.page;
-
-				if (num-by < 1) {
-					this.page.num = this.pages;
-					this.page.content = this.get(this.pages);
-				}
-				else {
-					this.page.num = num-by;
-					this.page.content = this.get(num-by);
-				}
-			}
-        });
+		
+    	this.contents: new Soup([ (obj.isPair()) ? Soup.from(Object) : Soup.from(Array) ]);
 
 
         var page = 0;
@@ -61,30 +23,67 @@ UtilService.newC("List", class {
 
 
         for (let i = 0; i < obj.length; i++) {
-                if (!paged.contents.get(page+1) && count >= per) { 
+                if (!this.contents.get(page+1) && count >= per) { 
                     page += 1; 
-                    paged.pages += 1; 
+                    this.pages += 1; 
 
-                    paged.contents.push( (obj.isPair()) ? Soup.from(Object) : Soup.from(Array) ); 
+                    this.contents.push( (obj.isPair()) ? Soup.from(Object) : Soup.from(Array) ); 
                     
                     count = 0;
                 }
 
 
-                if (obj.isPair()) paged.contents[page].push( ...obj.entries[i] );
-                else paged.contents[page].push( obj[i] );
+                if (obj.isPair()) this.contents[page].push( ...obj.entries[i] );
+                else this.contents[page].push( obj[i] );
 
-                paged.length += 1;
+                this.length += 1;
                 count += 1;
        
         }
 
 		
-		paged.page.content = paged.get(start);
+		this.page.content = this.get(start);
 
 		
         return paged;
     }
+
+	get(page) {
+		return this.contents[page-1];
+	}
+
+	to(page) {
+		this.page.num = page;
+		this.page.content = this.get(page);
+
+		return this.page;
+	}
+
+	next(by=1) {
+		let { num, content } = this.page;
+
+		if (num+by > this.pages) {
+			this.page.num = 1;
+			this.page.content = this.get(1);
+		}
+		else {
+			this.page.num = num+by;
+			this.page.content = this.get(num+by);
+		}
+	}
+
+	prev(by=1) {
+		let { num, content } = this.page;
+
+		if (num-by < 1) {
+			this.page.num = this.pages;
+			this.page.content = this.get(this.pages);
+		}
+		else {
+			this.page.num = num-by;
+			this.page.content = this.get(num-by);
+		}
+	}
 });
 
 
