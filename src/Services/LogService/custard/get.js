07fa355635010f338bed498dec34a8cb7dbafe78
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-30 13:31:13 -0500

    Update get.js

--- src/Services/LogService/custard/get.js
+++ src/Services/LogService/custard/get.js
@@ -1,1 +1,13 @@
+const LogService = require('../index.js');
 
+
+LogService.newF("get", async function(guild, settings) {
+    const client = this.parent;
+    client.import("util");
+
+    if (settings.before) settings.before = util.snowflake(settings.before);
+    if (settings.after) settings.after = util.snowflake(settings.after);
+    if (settings.type) settings.type = this.logType(settings.type);
+
+    return await guild.fetchLogs(settings).entries;
+})
