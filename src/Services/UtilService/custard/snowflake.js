b23f960e9acdd0a73b88ffae11dd15b3d2d3cdbb
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-30 13:22:26 -0500

    Update snowflake.js

--- src/Services/UtilService/custard/snowflake.js
+++ src/Services/UtilService/custard/snowflake.js
@@ -2,5 +2,6 @@ const UtilService = require('../index.js');
 
 
 UtilService.newF("snowflake", function(date=new Date()) {
+    if (date.constructor.name == "Timestamp") date = date.date;
     return ((BigInt(date.valueOf()) - BigInt(1420070400000)) << BigInt(22)).toString();
 });
