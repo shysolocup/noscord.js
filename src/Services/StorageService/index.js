49d696533814730b86a4f041da5e8bde6909287e
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-30 19:15:45 -0500

    Update index.js

--- src/Services/StorageService/index.js
+++ src/Services/StorageService/index.js
@@ -1,8 +1,17 @@
 const { Client } = require('../../Client');
+const { Soup } = require('stews');
 
 Client.newC("StorageService", class {
+    constructor() {
+        this.stores = new Soup(Object);
+    }
+    
     create(...args) {
         return new this.Store(...args);
+    }
+
+    get(name) {
+        return this.stores.get(name);
     }
 });
 
