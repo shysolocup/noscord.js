6e54c943ed71e278339d9b962e2ca71b36588871
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-31 10:26:20 -0500

    Update latency.js

--- src/Services/UtilService/custard/latency.js
+++ src/Services/UtilService/custard/latency.js
@@ -6,6 +6,5 @@ UtilService.newF("latency", function(msg, timestamp=null) {
 	
 	if (msg.constructor.name == "ChatInputCommandInteraction" || msg.constructor.name == "CommandRun") return this.date - msg.createdTimestamp
 	else if (msg.constructor.name == "Message") return timestamp.date - msg.timestamps.created.date;
-	else if (msg.constructor.name == "MessageEdit") return timestamp.date - msg.timestampes.edited.date;
 	else return timestamp.date - msg.createdAt;
 });
