22a396f5dd87c0b646e2f55c5adf1f9cd2b7b9ba
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-31 11:01:21 -0500

    Create index.js

---
+++ src/Services/TypeService/types/Emoji/index.js
@@ -0,0 +1,21 @@
+const TypeService = require('../../index.js');
+const fs = require('fs');
+
+TypeService.newC("Emoji", class {
+    [Symbol.toPrimitive](hint) {
+        if (hint === "string") {
+            return `<:${this.name}:${this.id}>`
+        }
+    }
+});
+
+module.exports = Emoji;
+
+/*
+let cust_dir = require('./custard/_funkydir');
+let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
+
+cust.forEach( (file) => {
+    require(`./custard/${file}`);
+});
+*/
