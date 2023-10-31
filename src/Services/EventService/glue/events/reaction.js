4e850dad1a7a7730356a8a0bf014c1728b984fb7
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-31 13:40:59 -0500

    Create reaction.js

---
+++ src/Services/EventService/glue/events/reaction.js
@@ -0,0 +1,15 @@
+module.exports = (handler) => { handler.init(
+
+    
+    name = ["reaction", "react"],  // names of the event
+
+    
+    func = function (...args) { // formatting for types and stuff
+        return args;
+    }, 
+
+    
+    glue = "messageReactionAdd" // what discord.js event it's tied to
+
+  
+)}
