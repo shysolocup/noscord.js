f47b2ff6d2e5d4142d1df5fa99881592c6b6a537
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-31 13:45:03 -0500

    Create leaveVoice.js

---
+++ src/Services/EventService/glue/events/leaveVoice.js
@@ -0,0 +1,18 @@
+module.exports = (handler) => { handler.init(
+
+    
+    name = "leaveVoice",  // names of the event
+
+    
+    func = async function (...args) { // formatting for types and stuff
+        return args;
+    }, 
+
+    
+    glue = "voiceStateUpdate", // what discord.js event it's tied to
+
+    
+    term = function(oldVS, newVS) { // specific action that triggers the event
+        return (!newVS.channelID && oldVS)
+    }
+)}
