845e9fd261be4cac1267c026a98c7f431a44cda1
paige <88659700+paigeroid@users.noreply.github.com>
2023-10-31 13:41:47 -0500

    Create removeReaction.js

---
+++ src/Services/EventService/glue/events/removeReaction.js
@@ -0,0 +1,15 @@
+module.exports = (handler) => { handler.init(
+
+    
+    name = ["reactionRemove", "removeReaction"],  // names of the event
+
+    
+    func = function (...args) { // formatting for types and stuff
+        return args;
+    }, 
+
+    
+    glue = "messageReactionRemove" // what discord.js event it's tied to
+
+  
+)}
