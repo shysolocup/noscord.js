ac7cbd527d253aec65212315b569124ef7e5be94
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-01 11:07:14 -0500

    Update get.js

--- src/Services/GuildService/custard/get.js
+++ src/Services/GuildService/custard/get.js
@@ -2,11 +2,17 @@ const GuildService = require('../index.js');
 
 
 GuildService.newF("get", async function(id) {
+    let client = this.parent;
+    client.import("types");
+    
     if (!id) return null; 
     
     let raw = id; 
     
     let thing = await this.parent._base.guilds.fetch(raw).catch(e=>{});
+    let typed = new types.Guild();
+
+    if (thing) await typed.apply(thing);
     
-    return (!thing) ? null : thing;
+    return (!thing) ? null : typed;
 });
