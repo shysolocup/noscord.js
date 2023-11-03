a9d4f6b0fffa12768e84bc969cd3bc69523d20bc
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-03 11:26:46 -0500

    Update index.js

--- src/Services/TypeService/types/UserGroup/index.js
+++ src/Services/TypeService/types/UserGroup/index.js
@@ -4,40 +4,17 @@ const fs = require('fs');
 
 
 TypeService.newC("UserGroup", class extends Soup {
-    constructor() {
+    constructor(guild) {
         super(Object);
 
-        const client = this.parent.parent;
-
-        return new Proxy(this, {
-            get(target, prop) {
-                try {
-                    if (Number(prop)+1 && Number(prop) <= target.length-1) {
-                        let id = target.keys[Number(prop)];
-                        return (async () => await client.users.get(id) )();
-                    }
-
-                    else if (target.has(prop)) {
-                        return (async () => await client.users.get(prop) )();
-                    }
-
-                    else return target[prop];
-
-                } catch(e) {
-                    return target[prop]
-                }
-            }
+        ( (guild) ? guild.raw.members : this.parent.parent._base.users )
+        
+        .fetch()
+        .catch(e=>{})
+        .then( (members) => {
+            members = Soup.from(members);
+            members.forEach( id => this.push(id, new this.UserPayload(id, guild) ) );
         });
-    }
-
-    async init(guild) {
-        this.insides = Soup.from(
-            await (
-                (guild) ? guild.raw.members : this.parent.parent._base.users
-            )
-            .fetch()
-            .catch(e=>{})
-        ).map( (id, base) => new this.UserPayload(id, guild) ).pour();
     }
 });
 
