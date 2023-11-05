a4e120b8b13432b071a13475a58d82d50fada750
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-04 23:05:07 -0500

    Create UserJail.js

---
+++ src/Services/TypeService/types/UserGroup/custard/UserJail.js
@@ -0,0 +1,15 @@
+const UserGroup = require('../index.js');
+const User = require('../../User');
+
+
+UserGroup.newC("UserJail", class {
+    constructor(id) { return (async () => {
+            const client = this.parent.parent.parent;
+            client.import("users");
+            
+            await client._base.users.get();
+
+            return id;
+        })();
+    }
+});
