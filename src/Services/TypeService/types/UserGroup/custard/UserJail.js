11f4b23e7865c04deb1a8371b294eeba9ae1781b
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-04 23:21:10 -0500

    Update UserJail.js

--- src/Services/TypeService/types/UserGroup/custard/UserJail.js
+++ src/Services/TypeService/types/UserGroup/custard/UserJail.js
@@ -1,16 +1,22 @@
 const UserGroup = require('../index.js');
+const deasync = require('deasync');
 const User = require('../../User');
 
 
 UserGroup.newC("UserJail", class {
-    constructor(id) { return (async () => {
-            const client = this.parent.parent.parent;
-            client.import("users");
+    constructor(id) {
+        const client = this.parent.parent.parent;
+        client.import("users");
 
-            this.id = id;
-            this.done = false;
-            
-            return await client._base.users.fetch(id);
-        })()
+        this.id = id;
+        this.done = false;
+
+        setImmediate( () => {
+            deasync.loopWhile( () => !this.done );  
+        });
+
+        return client._base.users.fetch(id).finally( () => {
+            this.done = true;
+        });
     }
 });
