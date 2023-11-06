dec4d6bce7411cf11af8fabc627559699f97aa02
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-06 13:02:13 -0600

    Create index.js

---
+++ src/Services/TypeService/types/GuildEmoji/index.js
@@ -0,0 +1,19 @@
+const TypeService = require('../../index.js');
+const fs = require('fs');
+
+TypeService.newC("GuildEmoji", class {
+    [Symbol.toPrimitive](hint) {
+        if (hint === "string") {
+            return `<:${this.name}:${this.id}>`
+        }
+    }
+});
+
+module.exports = GuildEmoji;
+
+let cust_dir = require('./custard/_funkydir');
+let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
+
+cust.forEach( (file) => {
+    require(`./custard/${file}`);
+});
