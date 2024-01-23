eae70aed286e513a80486bc4e15b7d557df84044
paige <88659700+paishee@users.noreply.github.com>
2024-01-23 17:52:54 -0600

    Update index.js

--- src/Services/TypeService/types/UserGroup/index.js
+++ src/Services/TypeService/types/UserGroup/index.js
@@ -2,29 +2,26 @@ const TypeService = require('../../index.js');
 const { Soup } = require('stews');
 const fs = require('fs');
 
+const pender = require('pender');
+
 
 TypeService.newC("UserGroup", class extends Soup {
     constructor(guild) {
         super(Object);
 
-        ( (guild) ? guild.raw.members : this.parent.parent._base.users )
+        const client = this.parent.parent;
+        client.import("users");
+
+        ( (guild) ? guild.raw.members : client._base.users )
         
         .fetch()
         .catch(e=>{})
         .then( (members) => {
             members = Soup.from(members);
-            members.forEach( (id, base) => this.push(id, new this.UserPayload(id, guild, base) ) );
+            members.forEach( (id, base) => this.push(id, pend( () => users.get(id, guild), `<@${id}>` )));
         });
     }
 });
 
 
 module.exports = UserGroup;
-
-
-let cust_dir = require('./custard/_funkydir');
-let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
-
-cust.forEach( (file) => {
-    require(`./custard/${file}`);
-});
