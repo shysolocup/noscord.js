da624a967dae5cb449e14b06c7e59db74dc7ca27
paige <88659700+paishee@users.noreply.github.com>
2024-01-24 10:16:29 -0600

    Update and rename index.js to index.js

--- src/Services/TypeService/types/SlashCommandRun/index.js
+++ src/Services/TypeService/types/SlashCommandRun/index.js
@@ -1,9 +1,9 @@
 const TypeService = require('../../index.js');
 const fs = require('fs');
 
-TypeService.newC("CommandRun");
+TypeService.newC("SlashCommandRun");
 
-module.exports = CommandRun;
+module.exports = SlashCommandRun;
 
 let cust_dir = require('./custard/_funkydir');
 let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
