68cc9d7717f43e12d1f0bee6715629e2a63502f6
paige <88659700+paishee@users.noreply.github.com>
2024-01-24 08:46:55 -0600

    Update index.js

--- src/Services/TypeService/types/TextChannelGroup/index.js
+++ src/Services/TypeService/types/TextChannelGroup/index.js
@@ -2,29 +2,20 @@ const TypeService = require('../../index.js');
 const { Soup } = require('stews');
 const fs = require('fs');
 
-const pend = require('pender');
-
 
 TypeService.newC("TextChannelGroup", class extends Soup {
     constructor(guild) {
         super(Object);
-
-        const client = this.parent.parent;
-        client.import("channels");
-
-        client._base.channels
-        
-        .fetch()
-        .catch(e=>{})
-        .then( stuff => {
-            stuff = Soup.from(stuff);
-            stuff.forEach( (id, base) => {
-                if (guild && base.guildId != guild.id) return;
-                if (base.type == 0) this.push(id, pend( () => channels.get(id, guild), `<#${id}>` ))
-            });
-        });
     }
 });
 
 
 module.exports = TextChannelGroup;
+
+
+let cust_dir = require('./custard/_funkydir');
+let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
+
+cust.forEach( (file) => {
+    require(`./custard/${file}`);
+});
