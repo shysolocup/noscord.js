080c2628a3136218547a8c2c86ade1ad43855265
paige <88659700+paishee@users.noreply.github.com>
2024-01-24 08:10:17 -0600

    Create index.js

---
+++ src/Services/TypeService/types/VoiceChannelGroup/index.js
@@ -0,0 +1,29 @@
+const TypeService = require('../../index.js');
+const { Soup } = require('stews');
+const fs = require('fs');
+
+const pend = require('pender');
+
+
+TypeService.newC("VoiceChannelGroup", class extends Soup {
+    constructor(guild) {
+        super(Object);
+
+        const client = this.parent.parent;
+        client.import("channels");
+
+        ( (guild) ? guild.raw.channels : client._base.channels )
+        
+        .fetch()
+        .catch(e=>{})
+        .then( stuff => {
+            stuff = Soup.from(stuff);
+            stuff.forEach( (id, base) => {
+                if (base.type == 3) this.push(id, pend( () => channels.get(id, guild), `<#${id}>` ))
+            });
+        });
+    }
+});
+
+
+module.exports = TextChannelGroup;
