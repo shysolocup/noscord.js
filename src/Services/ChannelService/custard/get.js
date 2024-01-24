a27c12adc546ae7dad5fcd3b8409697f24d48c0b
paige <88659700+paishee@users.noreply.github.com>
2024-01-23 18:55:40 -0600

    Update get.js

--- src/Services/ChannelService/custard/get.js
+++ src/Services/ChannelService/custard/get.js
@@ -1,18 +1,47 @@
 const ChannelService = require('../index.js');
 
 
-ChannelService.newF("get", async function(id) {
+ChannelService.newF("get", async function(id, guild=null) {
+	const client = this.parent;
+	client.import("types");
+	
     if (!id) return null; 
     
     let raw = id; 
     
     if (raw.startsWith('<#') && raw.endsWith('>')) {
         raw = raw.slice(2, -1);
-    } 
+    }
     
     raw = raw.split("").join(""); 
     
-    let thing = await this.parent._base.channels.fetch(raw).catch(e=>{});
-    
-    return (!thing) ? null : thing;
+    let thing = await ( 
+		(guild) ? 
+			guild.raw.channels : // if guild is given it gets from the guild
+		
+			this.parent._base.channels // if a guild is not given it gets from the client's channels
+		
+	).fetch(raw).catch(e=>{});
+
+    let typed
+
+    switch(thing.type) {
+        case 0: typed = new types.TextChannel; break;
+        // case 1: typed = new types.DmChannel; break;
+        case 2: typed = new types.VoiceChannel; break;
+        // case 3: typed = new types.GroupDmChannel; break;
+        // case 4: typed = new types.Category; break;
+        // case 5: typed = new types.AnnouncementsChannel; break;
+        // case 10: typed = new types.AnnouncementsThread; break;
+        // case 11: typed = new types.PublicThread; break;
+        // case 12: typed = new types.PrivateThread; break;
+        // case 13: typed = new types.StageChannel; break;
+        // case 14: typed = new types.HubChannel; break;
+        // case 15: typed = new types.ForumChannel; break;
+        // case 16: typed = new types.MediaChannel; break;
+    }
+
+	if (thing) await typed.apply(thing);
+ 
+    return (!thing) ? null : typed;
 });
