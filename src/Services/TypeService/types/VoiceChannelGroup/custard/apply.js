ace450ad2a982b453911dbc5dcfb64392ba7010e
paige <88659700+paishee@users.noreply.github.com>
2024-01-24 09:20:01 -0600

    Create apply.js

---
+++ src/Services/TypeService/types/VoiceChannelGroup/custard/apply.js
@@ -0,0 +1,31 @@
+const { Soup } = require('stews');
+const VoiceChannelGroup = require('../index.js');
+const pend = require('pender');
+
+
+VoiceChannelGroup.newF("apply", async function(guild) {
+    const client = this.parent.parent;
+    client.import("guilds", "channels");
+
+    let stuff  = new Soup(Object);
+
+    if (!guild) {
+        let gList = Soup.from( await client._base.guilds.fetch() );
+
+        for ( let i = 0; i < gList.length; i++) {
+            let guild = await guilds.get( gList.keys[i] );
+            let chList = await guild.raw.channels.fetch().catch(e=>console.log(e))
+            stuff = stuff.merge( Soup.from(chList));
+        }
+    }
+    else {
+        let chList = await guild.raw.channels.fetch().catch(e=>console.log(e))
+        stuff = Soup.from(chList);
+    }
+
+
+    stuff.forEach( (id, base) => {
+        if (guild && base.guildId != guild.id) return;
+        if (base.type == 2) this.push(id, pend( () => channels.get(id, guild), `<#${id}>` ))
+    });
+})
