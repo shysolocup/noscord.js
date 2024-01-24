5d1d50666a23f465b90c23ed49a15d082996a0bd
paige <88659700+paishee@users.noreply.github.com>
2024-01-24 08:12:46 -0600

    Update index.js

--- src/Services/TypeService/types/VoiceChannelGroup/index.js
+++ src/Services/TypeService/types/VoiceChannelGroup/index.js
@@ -12,18 +12,19 @@ TypeService.newC("VoiceChannelGroup", class extends Soup {
         const client = this.parent.parent;
         client.import("channels");
 
-        ( (guild) ? guild.raw.channels : client._base.channels )
+        client._base.channels
         
         .fetch()
         .catch(e=>{})
         .then( stuff => {
             stuff = Soup.from(stuff);
             stuff.forEach( (id, base) => {
-                if (base.type == 3) this.push(id, pend( () => channels.get(id, guild), `<#${id}>` ))
+                if (guild && base.guildId != guild.id) return;
+                if (base.type == 2) this.push(id, pend( () => channels.get(id, guild), `<#${id}>` ))
             });
         });
     }
 });
 
 
-module.exports = TextChannelGroup;
+module.exports = VoiceChannelGroup;
