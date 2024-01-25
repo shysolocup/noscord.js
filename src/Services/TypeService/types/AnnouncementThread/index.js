3c19cfa48201159ba19128490cf3227d4d44681f
paige <88659700+paishee@users.noreply.github.com>
2024-01-25 09:32:56 -0600

    Create index.js

---
+++ src/Services/TypeService/types/AnnouncementThread/index.js
@@ -0,0 +1,19 @@
+const TypeService = require('../../index.js');
+const fs = require('fs');
+
+TypeService.newC("AnnouncementThread", class {
+    [Symbol.toPrimitive](hint) {
+        if (hint === "string") {
+            return `<#${this.id}>`
+        }
+    }
+});
+
+module.exports = AnnouncementThread;
+
+let cust_dir = require('./custard/_funkydir');
+let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
+
+cust.forEach( (file) => {
+    require(`./custard/${file}`);
+});
