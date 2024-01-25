341949692f02a802ee98018417ee007331f0ac03
paige <88659700+paishee@users.noreply.github.com>
2024-01-25 13:20:36 -0600

    Create ruleDelete.js

---
+++ src/Services/EventService/glue/events/ruleDelete.js
@@ -0,0 +1,15 @@
+module.exports = (handler) => { handler.init(
+
+    
+    name = [ "ruleDelete", "autoModerationRuleDelete" ],  // name of the event
+
+    
+    func = async function (...args) { // formatting for types and stuff
+        return args;
+    }, 
+
+    
+    glue = "autoModerationRuleDelete" // what discord.js event it's tied to
+
+    
+)}
