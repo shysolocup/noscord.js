eb719a0fad8f5ace20a50137fc4ad3ce6fce77d2
paige <88659700+paishee@users.noreply.github.com>
2024-01-25 12:55:02 -0600

    Create channelCreate.js

---
+++ src/Services/EventService/glue/events/channelCreate.js
@@ -0,0 +1,18 @@
+module.exports = (handler) => { handler.init(
+
+    
+    name = "channelCreate",  // names of the event
+
+    
+    func = async function (ctx) { // formatting for types and stuff
+        let types = this.types;
+        let action = new types.VictimChannel;
+        
+        await action.apply(...Array.from(arguments));
+        
+        return [action, action.moderator, action.guild];
+    }, 
+
+    
+    glue = "channelCreate", // what discord.js event it's tied to
+)}
