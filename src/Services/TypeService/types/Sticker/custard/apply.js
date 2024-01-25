0deba5d48fe0a5e688de356b02d979164a5e77e9
paige <88659700+paishee@users.noreply.github.com>
2024-01-25 08:36:45 -0600

    Create apply.js

---
+++ src/Services/TypeService/types/Sticker/custard/apply.js
@@ -0,0 +1,32 @@
+const Sticker = require('../index.js');
+
+
+Sticker.newF("apply", async function(base) {
+    const client = this.parent.parent;
+    client.import({ att: "attachments" }, "util", "guilds" );
+
+    // main info
+    this.id = base.id;
+    this.name = base.name;
+    this.description = base.description;
+    this.type = base.type;
+    this.url = base.url;
+    this.attachment = await att.create(this.url);
+
+    // guild
+    this.guild = await guilds.get(base.guildId);
+    this.guildId = base.guildId;
+    
+    // random stuff
+    this.tags = base.tags;
+    this.packId = base.packId;
+    this.available = base.available;
+    this.format = base.format;
+    this.partial = base.partial;
+
+    // timestamps
+    this.createdAt = base.createdAt;
+    this.timestamps = {
+        created: new Timestamp(base.createdAt)
+    };
+})
