9d065366f8f7a4ee441507285f6cded67aaaae7b
paige <88659700+paishee@users.noreply.github.com>
2024-01-25 13:18:52 -0600

    Create automod.js

---
+++ src/Services/EventService/glue/events/automod.js
@@ -0,0 +1,15 @@
+module.exports = (handler) => { handler.init(
+
+    
+    name = [ "autmod", "autoModerationActionExecution" ],  // name of the event
+
+    
+    func = async function (...args) { // formatting for types and stuff
+        return args;
+    }, 
+
+    
+    glue = "autoModerationActionExecution" // what discord.js event it's tied to
+
+    
+)}
