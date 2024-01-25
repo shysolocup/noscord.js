4ce4572d3326c5fa9d9f7ce194af4e17a3c8e377
paige <88659700+paishee@users.noreply.github.com>
2024-01-25 08:31:44 -0600

    Update index.js

--- src/Services/TypeService/types/Sticker/index.js
+++ src/Services/TypeService/types/Sticker/index.js
@@ -11,11 +11,9 @@ TypeService.newC("Sticker", class {
 
 module.exports = Sticker;
 
-/*
 let cust_dir = require('./custard/_funkydir');
 let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
 
 cust.forEach( (file) => {
     require(`./custard/${file}`);
 });
-*/
