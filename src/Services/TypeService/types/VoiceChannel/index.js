cba7fd3a1e7182c69d39a8e47199e91cd8ed9c76
paige <88659700+paishee@users.noreply.github.com>
2024-01-25 08:41:01 -0600

    Update index.js

--- src/Services/TypeService/types/VoiceChannel/index.js
+++ src/Services/TypeService/types/VoiceChannel/index.js
@@ -11,11 +11,9 @@ TypeService.newC("VoiceChannel", class {
 
 module.exports = VoiceChannel;
 
-/*
 let cust_dir = require('./custard/_funkydir');
 let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
 
 cust.forEach( (file) => {
     require(`./custard/${file}`);
 });
-*/
