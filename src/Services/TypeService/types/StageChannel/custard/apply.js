1caad87c16fd1e9d9deaa3ec0b3c8bf19f64dfb7
paige <88659700+paishee@users.noreply.github.com>
2024-01-25 09:44:37 -0600

    Create apply.js

---
+++ src/Services/TypeService/types/StageChannel/custard/apply.js
@@ -0,0 +1,43 @@
+const StageChannel = require('../index.js');
+const pend = require('pender');
+
+
+StageChannel.newF("apply", async function(ctx) {
+    const client = this.parent.parent;
+    client.import("messages", "channels", "guilds", "util", "types");
+
+    // information
+    let base = new types.BaseChannel;
+	base.apply(ctx);
+
+	Object.assign(this, base);
+
+
+    // stuff
+    this.guild = pend( async () => await guilds.get(ctx.guildId), `<Guild:${ctx.guildId}>` );
+    this.flags = ctx.flags;
+    this.partial = ctx.partial;
+    this.permissionOverwrites = ctx.permissionOverwrites;
+    this.permissionsLocked = ctx.permissionsLocked;
+    this.position = ctx.position;
+    this.rateLimitPerUser = ctx.rateLimitPerUser;
+    this.rawPosition = ctx.rawPosition;
+    // this.threads // await channels.threads(ctx);
+
+
+    // messages
+    // this.messages = ctx.messages;
+    // this.msgList // = await messages.list(ctx);
+    // this.lastMsg // = await messages.get(ctx.lastMessageId);
+    // this.lastMsgId = ctx.lastMessageId;
+
+
+    // times
+    // this.timestamps.lastPin = new Timestamp(ctx.lastPinAt)
+
+
+    Object.defineProperty(this, "raw", {
+		get() { return ctx }	
+	});
+
+});
