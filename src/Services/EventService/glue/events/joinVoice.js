370ba35a93b393f5b6b40a2f790239261ec85a78
paige <88659700+paishee@users.noreply.github.com>
2024-01-25 11:10:50 -0600

    Update joinVoice.js

--- src/Services/EventService/glue/events/joinVoice.js
+++ src/Services/EventService/glue/events/joinVoice.js
@@ -13,6 +13,6 @@ module.exports = (handler) => { handler.init(
 
     
     term = function(oldVS, newVS) { // specific action that triggers the event
-        return (!oldVS.channelID && newVS)
+        return (!oldVS.channelID && newVS.channelId)
     }
 )}
