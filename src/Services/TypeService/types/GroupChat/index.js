803f1f4611ce1bc68618362ad737c3fb65bcc629
paige <88659700+paishee@users.noreply.github.com>
2024-01-25 09:37:58 -0600

    Update and rename index.js to index.js

--- src/Services/TypeService/types/GroupChat/index.js
+++ src/Services/TypeService/types/GroupChat/index.js
@@ -1,7 +1,7 @@
 const TypeService = require('../../index.js');
 const fs = require('fs');
 
-TypeService.newC("GroupChatChannel", class {
+TypeService.newC("GroupChat", class {
     [Symbol.toPrimitive](hint) {
         if (hint === "string") {
             return `<#${this.id}>`
@@ -9,7 +9,7 @@ TypeService.newC("GroupChatChannel", class {
     }
 });
 
-module.exports = GroupChatChannel;
+module.exports = GroupChat;
 
 let cust_dir = require('./custard/_funkydir');
 let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
