abe154635cf11ad205b9815c5e9cec8d6c1acfff
paige <88659700+paishee@users.noreply.github.com>
2024-01-25 13:20:11 -0600

    Create ruleEdit.js

---
+++ src/Services/EventService/glue/events/ruleEdit.js
@@ -0,0 +1,15 @@
+module.exports = (handler) => { handler.init(
+
+    
+    name = [ "ruleEdit", "ruleUpdate", "autoModerationRuleUpdate" ],  // name of the event
+
+    
+    func = async function (...args) { // formatting for types and stuff
+        return args;
+    }, 
+
+    
+    glue = "autoModerationRuleUpdate" // what discord.js event it's tied to
+
+    
+)}
