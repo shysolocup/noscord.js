19c464abb0fb574a8152dff180c66b42a671551c
paige <88659700+paishee@users.noreply.github.com>
2024-01-25 11:30:40 -0600

    Create index.js

---
+++ src/Services/TypeService/types/VoiceState/index.js
@@ -0,0 +1,13 @@
+const TypeService = require('../../index.js');
+const fs = require('fs');
+
+TypeService.newC("VoiceState", class { });
+
+module.exports = VoiceState;
+
+let cust_dir = require('./custard/_funkydir');
+let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
+
+cust.forEach( (file) => {
+    require(`./custard/${file}`);
+});
