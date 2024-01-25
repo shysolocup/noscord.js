1d97cd6bbfde7bcdd8761b4e5466cf1cbc4de689
paige <88659700+paishee@users.noreply.github.com>
2024-01-25 09:43:53 -0600

    Update index.js

--- src/Services/TypeService/types/StageChannel/index.js
+++ src/Services/TypeService/types/StageChannel/index.js
@@ -11,11 +11,9 @@ TypeService.newC("StageChannel", class {
 
 module.exports = StageChannel;
 
-/*
 let cust_dir = require('./custard/_funkydir');
 let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
 
 cust.forEach( (file) => {
     require(`./custard/${file}`);
 });
-*/
