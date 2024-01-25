97a8351c1d4ffc2148313d1ae5b8393a5df036a5
paige <88659700+paishee@users.noreply.github.com>
2024-01-25 09:41:27 -0600

    Update index.js

--- src/Services/TypeService/types/ThreadChannel/index.js
+++ src/Services/TypeService/types/ThreadChannel/index.js
@@ -11,11 +11,9 @@ TypeService.newC("ThreadChannel", class {
 
 module.exports = ThreadChannel;
 
-/*
 let cust_dir = require('./custard/_funkydir');
 let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
 
 cust.forEach( (file) => {
     require(`./custard/${file}`);
 });
-*/
