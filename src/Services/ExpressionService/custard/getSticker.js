2968ab396bb89a9410559bdec80d6d686f48583c
paige <88659700+paishee@users.noreply.github.com>
2024-01-25 08:39:28 -0600

    Create getSticker.js

---
+++ src/Services/ExpressionService/custard/getSticker.js
@@ -0,0 +1,17 @@
+const ExpressionService = require('../index.js');
+
+
+ExpressionService.newF("getSticker", async function(id, guild) {
+	const client = this.parent;
+	client.import("types");
+	
+    if (!id) return null; 
+    
+    let thing = await guild.raw.stickers
+        .fetch(id).catch(e=>{});
+
+	let typed = new types.Sticker
+	if (thing) await typed.apply(thing);
+ 
+    return (!thing) ? null : typed;
+});
