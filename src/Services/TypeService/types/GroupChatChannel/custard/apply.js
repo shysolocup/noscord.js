dc449689fb314146041c094254243103f377d6f1
paige <88659700+paishee@users.noreply.github.com>
2024-01-25 09:28:54 -0600

    Create apply.js

---
+++ src/Services/TypeService/types/GroupChatChannel/custard/apply.js
@@ -0,0 +1,31 @@
+const GroupChatChannel = require('../index.js');
+const pend = require('pender');
+
+
+GroupChatChannel.newF("apply", async function(ctx) {
+    const client = this.parent.parent;
+    client.import("messages", "util", "types");
+
+    // information
+    let base = new types.BaseChannel;
+	base.apply(ctx);
+
+	Object.assign(this, base);
+
+
+    // messages
+    this.messages = ctx.messages;
+    this.msgList // = await messages.list(ctx);
+    this.lastMsg // = await messages.get(ctx.lastMessageId);
+    this.lastMsgId = ctx.lastMessageId;
+
+
+    // times
+    this.timestamps.lastPin = new Timestamp(ctx.lastPinAt)
+
+
+    Object.defineProperty(this, "raw", {
+		get() { return ctx }	
+	});
+
+});
