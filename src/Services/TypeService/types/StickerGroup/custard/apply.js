7540eb7bdaa71580f3e3e9a81fd2656722bc4efa
paige <88659700+paishee@users.noreply.github.com>
2024-01-25 08:30:26 -0600

    Create apply.js

---
+++ src/Services/TypeService/types/StickerGroup/custard/apply.js
@@ -0,0 +1,44 @@
+const { Soup } = require('stews');
+const StickerGroup = require('../index.js');
+const pend = require('pender');
+
+
+StickerGroup.newF("apply", async function(guild) {
+    const client = this.parent.parent;
+    client.import("guilds", "types");
+
+    let stuff = new Soup(Object);
+    let bases = new Soup(Object);
+
+    if (!guild) {
+        let gList = Soup.from( await client._base.guilds.fetch() );
+
+        for ( let i = 0; i < gList.length; i++) {
+            let guild = gList.values[i];
+            let list = await guild.stickers.fetch().catch(e=>console.log(e))
+            stuff = stuff.merge( Soup.from(list));
+        }
+    }
+    else {
+        let list = await guild.raw.stickers.fetch().catch(e=>console.log(e))
+        stuff = Soup.from(list);
+    }
+
+
+    stuff.forEach( (id, base) => {
+        bases.push(id, base);
+        let stk = new types.Sticker;
+        this.push(id, pend( async () => {
+            
+            await stk.apply(base);
+            return stk;
+            
+        }, `<S:${id}>` ));
+    });
+
+    Object.defineProperties(this, {
+        static: { get: () => this.filter( (id) => !bases[id].animated ) },
+        animated: { get: () => this.filter( (id) => bases[id].animated ) },
+        named: { value: (name) => this.filter( (id) => bases[id].name == name ) }
+    })
+})
