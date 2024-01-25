4915833419b35be320ea88c93b76f3a2ca9fba2e
paige <88659700+paishee@users.noreply.github.com>
2024-01-25 13:05:28 -0600

    Update and rename index.js to index.js

--- src/Services/TypeService/types/VictimMember/index.js
+++ src/Services/TypeService/types/VictimMember/index.js
@@ -1,7 +1,7 @@
 const TypeService = require('../../index.js');
 const fs = require('fs');
 
-TypeService.newC("GuildMemberAction", class {
+TypeService.newC("VictimMember", class {
     [Symbol.toPrimitive](hint) {
         if (hint === "string") {
             return `<@${this.victim.id}>`;
@@ -9,7 +9,7 @@ TypeService.newC("GuildMemberAction", class {
     }
 });
 
-module.exports = GuildMemberAction;
+module.exports = VictimMember;
 
 let cust_dir = require('./custard/_funkydir');
 let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
