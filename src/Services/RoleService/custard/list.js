494b311541ed3a866d724f3fa22425dd49824bc3
paige <88659700+paishee@users.noreply.github.com>
2024-01-26 10:33:10 -0600

    Update list.js

--- src/Services/RoleService/custard/list.js
+++ src/Services/RoleService/custard/list.js
@@ -2,5 +2,5 @@ const RoleService = require('../index.js');
 
 
 RoleService.newF("list", async function(guild) {
-    return Soup.from(await guild.roles.fetch()).pour();
+    return Soup.from(await ((guild.raw) ? guild.raw : guild) .roles.fetch()).pour();
 });
