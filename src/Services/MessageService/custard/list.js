d35219b74fabd81dbe8976d4703207bcdc9d3959
paige <88659700+paishee@users.noreply.github.com>
2024-01-26 10:25:51 -0600

    Update list.js

--- src/Services/MessageService/custard/list.js
+++ src/Services/MessageService/custard/list.js
@@ -2,5 +2,5 @@ const MessageService = require('../index.js');
 
 
 MessageService.newF("list", async function(channel, limit=100) {
-    return Soup.from(await channel.messages.fetch({ limit: limit })).pour();
+    return Soup.from(await ((channel.raw) ? channel.raw : channel) .messages.fetch({ limit: limit })).pour();
 });
