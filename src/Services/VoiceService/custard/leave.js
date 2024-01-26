1f0321f870c291189c20b1b905596740afd67b6f
paige <88659700+paishee@users.noreply.github.com>
2024-01-26 08:30:08 -0600

    Update leave.js

--- src/Services/VoiceService/custard/leave.js
+++ src/Services/VoiceService/custard/leave.js
@@ -3,7 +3,7 @@ const voice = require('@discordjs/voice');
 
 
 VoiceService.newF("leave", function(channel) {
-    const connection = voice.getVoiceConnection(channel.guild.id);
+    const connection = voice.getVoiceConnection(channel.guildId);
 	try { connection.destroy(); } catch(err) {}
 	return connection;
 })
