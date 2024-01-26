ec3dd689c405c8c4a4cb399a325ffd3e1ce2e5fc
paige <88659700+paishee@users.noreply.github.com>
2024-01-26 11:11:16 -0600

    Update apply.js

--- src/Services/TypeService/types/VictimChannel/custard/apply.js
+++ src/Services/TypeService/types/VictimChannel/custard/apply.js
@@ -5,23 +5,7 @@ VictimChannel.newF("apply", async function(ctx, actionType=null) {
     const client = this.parent.parent;
     client.import("types", "messages", "channels", "guilds", "util");
 
-	let channel;
-
-	switch(ctx.type) {
-        case 0: channel = new types.TextChannel; break;
-        case 1: channel = new types.DirectMessage; break;
-        case 2: channel = new types.VoiceChannel; break;
-        case 3: channel = new types.GroupChat; break;
-        case 4: channel = new types.Category; break;
-        case 5: channel = new types.AnnouncementsChannel; break;
-        case 10: channel = new types.AnnouncementsThread; break;
-        case 11: channel = new types.ThreadChannel; break;
-        case 12: channel = new types.ThreadChannel; break;
-        case 13: channel = new types.StageChannel; break;
-        // case 14: channel = new types.HubChannel; break;
-        // case 15: channel = new types.ForumChannel; break;
-        // case 16: channel = new types.MediaChannel; break;
-    }
+	let channel = channels.typer(ctx.type);
 
 	await channel.apply(ctx);
 
