9eebda0db3205328a816ddcbe4c3a7f30d8b1d00
paige <88659700+paishee@users.noreply.github.com>
2024-01-26 10:58:30 -0600

    Update apply.js

--- src/Services/TypeService/types/TextChannel/custard/apply.js
+++ src/Services/TypeService/types/TextChannel/custard/apply.js
@@ -8,7 +8,7 @@ TextChannel.newF("apply", async function(ctx) {
 
     // information
     let base = new types.BaseChannel;
-	base.apply(ctx);
+	await base.apply(ctx);
 
 	Object.assign(this, base);
 
