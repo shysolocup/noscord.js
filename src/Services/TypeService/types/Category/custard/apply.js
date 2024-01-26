88a6b8f4ef257f8c46cac82c9817204b9b969209
paige <88659700+paishee@users.noreply.github.com>
2024-01-26 08:20:02 -0600

    Update apply.js

--- src/Services/TypeService/types/Category/custard/apply.js
+++ src/Services/TypeService/types/Category/custard/apply.js
@@ -6,12 +6,33 @@ Category.newF("apply", async function(ctx) {
     const client = this.parent.parent;
     client.import("messages", "channels", "guilds", "util", "types");
 
+	
     // information
     let base = new types.TextChannel;
 	base.apply(ctx);
 
 	Object.assign(this, base);
 
+	
+	this.children = ctx.children;
+	this.guild = await guilds.get(ctx.guildId);
+
+	
+	// groups
+	this.groups = pend( async () => {
+        let stuff = new Soup({
+            members: new types.MemberGroup(),
+            channels: new types.ChannelGroup(),
+        });
+
+        for (let i = 0; i < stuff.length; i++) {
+            await stuff.values[i].apply(this.guild, ctx);
+        }
+
+        return stuff
+    }, "ChildGroups" );
+
+	
     Object.defineProperty(this, "raw", {
 		get() { return ctx }	
 	});
