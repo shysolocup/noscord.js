76ee1b210ce8337c7d3a5a6938ab2193278fd201
paige <88659700+paishee@users.noreply.github.com>
2024-01-26 10:19:47 -0600

    Update threads.js

--- src/Services/ChannelService/custard/threads.js
+++ src/Services/ChannelService/custard/threads.js
@@ -3,10 +3,10 @@ const { Soup } = require('stews');
 
 
 ChannelService.newF("threads", async function(guild=null) {
-    return new Soup( await ( (guild) ? guild.raw.channels : this.parent._base.channels)
+    return new Soup( await ( (guild) ? ((guild.raw) ? guild.raw : guild) .channels : this.parent._base.channels)
         .fetch()
         .catch(e=>{})
     )
-		.filter( (id, channel) => { return channel.type == 11; })
+		.filter( (id, channel) => { return channel.type == 11 || channel.type == 12; })
 		.pour();
 });
