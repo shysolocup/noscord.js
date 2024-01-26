b10721b7b3beb5b0680da5396f38485f6f733bf9
paige <88659700+paishee@users.noreply.github.com>
2024-01-26 12:28:09 -0600

    Update apply.js

--- src/Services/TypeService/types/VoiceMemberGroup/custard/apply.js
+++ src/Services/TypeService/types/VoiceMemberGroup/custard/apply.js
@@ -21,6 +21,9 @@ VoiceMemberGroup.newF("apply", async function(channel) {
 
 
     Object.defineProperties(this, {
+        usernames: { get: () => this.map( id => bases[id].user.username ) },
+        nicknames: { get: () => this.map( id => { let name = bases[id].displayName; return (name) ? name : bases[id].user.username } ) },
+        
         users: { get: () => this.filter( (id) => !bases[id].user.bot ) },
         bots: { get: () => this.filter( (id) => bases[id].user.bot ) },
         
