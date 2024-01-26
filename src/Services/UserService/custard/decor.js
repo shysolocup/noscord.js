f81003535d8ce69772567f4866e3feca9b364a67
paige <88659700+paishee@users.noreply.github.com>
2024-01-26 10:35:44 -0600

    Update decor.js

--- src/Services/UserService/custard/decor.js
+++ src/Services/UserService/custard/decor.js
@@ -5,7 +5,7 @@ UserService.newF("decor", async function(user, settings={}) {
     const client = this.parent;
     client.import({ att: "attachments" }, "util")
 
-    let decorUrl = user.avatarDecorationURL(settings.dynamic);
+    let decorUrl = ((user.raw) ? user.raw : user) .avatarDecorationURL(settings.dynamic);
 
     if (!settings.name) settings.name = util.genCode(9);
 
