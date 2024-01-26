91a93159b065f1415bab27beb6a3b573248a65a2
paige <88659700+paishee@users.noreply.github.com>
2024-01-26 10:22:47 -0600

    Update emojis.js

--- src/Services/ExpressionService/custard/emojis.js
+++ src/Services/ExpressionService/custard/emojis.js
@@ -3,5 +3,5 @@ const { Soup } = require('stews');
 
 
 ExpressionService.newF("emojis", async function(guild) {
-    return Soup.from(await guild.emojis.fetch()).pour();
+    return Soup.from(await ((guild.raw) ? guild.raw : guild) .emojis.fetch()).pour();
 });
