dd0305ec6aabc3f4d5d26e7d962fe2b152612b08
paige <88659700+paishee@users.noreply.github.com>
2024-01-26 08:48:30 -0600

    Update apply.js

--- src/Services/TypeService/types/VoiceChannel/custard/apply.js
+++ src/Services/TypeService/types/VoiceChannel/custard/apply.js
@@ -11,32 +11,9 @@ VoiceChannel.newF("apply", async function(ctx) {
 	base.apply(ctx);
 
 	Object.assign(this, base);
-	
 
     // stuff
 	this.members = new types.VoiceMemberGroup;
-    this.guild = await guilds.get(ctx.guildId);
-	this.guildId = ctx.guildId;
-    this.flags = ctx.flags;
-    this.partial = ctx.partial;
-    this.permissionOverwrites = ctx.permissionOverwrites;
-    this.permissionsLocked = ctx.permissionsLocked;
-    this.position = ctx.position;
-    this.rateLimitPerUser = ctx.rateLimitPerUser;
-    this.rawPosition = ctx.rawPosition;
-    // this.threads // await channels.threads(ctx);
-
-
-    // messages
-    // this.messages = ctx.messages;
-    // this.msgList // = await messages.list(ctx);
-    // this.lastMsg // = await messages.get(ctx.lastMessageId);
-    // this.lastMsgId = ctx.lastMessageId;
-
-
-    // times
-    // this.timestamps.lastPin = new Timestamp(ctx.lastPinAt)
-
 
     Object.defineProperty(this, "raw", {
 		get() { return ctx }	
