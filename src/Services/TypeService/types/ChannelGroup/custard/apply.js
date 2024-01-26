c6114e5bf29a7ce27016cd3e3108cc042e4b0812
paige <88659700+paishee@users.noreply.github.com>
2024-01-26 08:12:13 -0600

    Update apply.js

--- src/Services/TypeService/types/ChannelGroup/custard/apply.js
+++ src/Services/TypeService/types/ChannelGroup/custard/apply.js
@@ -3,14 +3,19 @@ const ChannelGroup = require('../index.js');
 const pend = require('pender');
 
 
-ChannelGroup.newF("apply", async function(guild) {
+ChannelGroup.newF("apply", async function(guild, category=null) {
     const client = this.parent.parent;
     client.import("guilds", "channels");
 
     let stuff = new Soup(Object);
     let bases = new Soup(Object);
 
-    if (!guild) {
+
+    if (category) {
+        let list = await ((category.raw) ? category.raw : category) .channels.fetch().catch(e=>console.log(e))
+        stuff = Soup.from(list);
+    }
+    else if (!guild) {
         let gList = Soup.from( await client._base.guilds.fetch() );
 
         for ( let i = 0; i < gList.length; i++) {
