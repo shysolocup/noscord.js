60aa88507960b1f34e80cd0549757e12103d8fa9
paige <88659700+paishee@users.noreply.github.com>
2024-01-29 15:04:35 -0600

    Update apply.js

--- src/Services/TypeService/types/Guild/custard/apply.js
+++ src/Services/TypeService/types/Guild/custard/apply.js
@@ -50,7 +50,7 @@ Guild.newF("apply", async function(ctx) {
 	});
 
 	Object.defineProperties( this, {
-		members: get: () => {
+		members: { get: () => {
 			let stuff = new types.MemberGroup;
 
 			( async () => {
@@ -58,7 +58,7 @@ Guild.newF("apply", async function(ctx) {
 			})();
 
 			return stuff;
-		}
+		}}
 	});
 
     this.groups = pend( async () => {
