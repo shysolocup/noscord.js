f623b5908b5973d4cbd34efc014bc5b4c35fbdf7
paige <88659700+paishee@users.noreply.github.com>
2024-01-29 08:22:31 -0600

    Update messagePin.js

--- src/Services/EventService/glue/events/messagePin.js
+++ src/Services/EventService/glue/events/messagePin.js
@@ -1,7 +1,7 @@
 module.exports = (handler) => { handler.init(
 
     
-    name = ["messagePin", "pin"],  // names of the event
+    name = ["messagePin", "pin", "messagePinned", "pinned"],  // names of the event
 
     
     func = async function (ctx) { // formatting for types and stuff
