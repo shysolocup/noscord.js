45dcdeaf5fa2c2dea970827d3c23015d423cb29b
paige <88659700+paishee@users.noreply.github.com>
2024-01-29 08:14:11 -0600

    Create messagePin.js

---
+++ src/Services/EventService/glue/events/messagePin.js
@@ -0,0 +1,24 @@
+module.exports = (handler) => { handler.init(
+
+    
+    name = ["messagePin", "pin"],  // names of the event
+
+    
+    func = async function (ctx) { // formatting for types and stuff
+        let client = this.parent.parent;
+        client.import("types", "channels");
+
+        let channel = channels.typer(ctx.type);
+        let msgType = new types.Message;
+
+        let msg = (await ctx.messages.fetchPinned()).first();
+        
+        await channel.apply(ctx);
+        await msgType.apply(msg);
+        
+        return [msgType, channel];
+    }, 
+
+    
+    glue = "channelPinsUpdate", // what discord.js event it's tied to
+)}
