dd38eb31a85f6fd3bb706e09dac6525a503524f1
paige <88659700+paishee@users.noreply.github.com>
2024-01-29 10:10:10 -0600

    Update all.js

--- src/Services/ChannelService/custard/all.js
+++ src/Services/ChannelService/custard/all.js
@@ -3,5 +3,16 @@ const { Soup } = require('stews');
 
 
 ChannelService.newF("all", async function() {
-    return Soup.from(await this.parent._base.channels.fetch().catch(e=>{}));
+    const client = this.parent;
+    client.import("types", "channels");
+    
+    let stuff = Soup.from( await this.parent._base.channels.fetch())
+    for (let [id, raw] of stuff) {
+        let thing = channels.typer(raw.type);
+        await thing.apply(raw);
+
+        stuff.set(id, thing);
+    }
+
+    return stuff;
 });
