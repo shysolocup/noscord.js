7a32ef72b1eb9338e1c0d44e017f50a2471200a6
paige <88659700+paishee@users.noreply.github.com>
2024-01-29 11:55:13 -0600

    Update list.js

--- src/Services/UserService/custard/list.js
+++ src/Services/UserService/custard/list.js
@@ -3,17 +3,11 @@ const { Soup } = require('stews');
 
 
 UserService.newF("list", async function(guild=null) {
-    let users = new Soup( await ( (guild) ? ((user.raw) ? user.raw : user) .members : this.parent._base.users)
-        .fetch()
-        .catch(e=>{})
-    );
+    const client = this.parent;
+    client.import("types");
+    
+   	let group = new types.MemberGroup;
+	await group.apply(guild);
 
-	
-	for (let i = 0; i < users.length; i++) {
-		users[i] = await this.get(users.keys[i]);
-	}
-	
-
-	return users
-		.pour();
+    return group;
 });
