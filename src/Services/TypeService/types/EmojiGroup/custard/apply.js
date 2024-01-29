1d1524e2cc55cdc7dcf498e1ca03b018719a42b0
paige <88659700+paishee@users.noreply.github.com>
2024-01-29 10:24:05 -0600

    Update apply.js

--- src/Services/TypeService/types/EmojiGroup/custard/apply.js
+++ src/Services/TypeService/types/EmojiGroup/custard/apply.js
@@ -13,8 +13,8 @@ EmojiGroup.newF("apply", async function(guild) {
     if (!guild) {
         let gList = Soup.from( await client._base.guilds.fetch() );
 
-        for ( let i = 0; i < gList.length; i++) {
-            let guild = gList.values[i];
+        for ( let [id, guild] of gList ) {
+            guild = await guild.fetch();
             let list = await guild.emojis.fetch().catch(e=>console.log(e))
             stuff = stuff.merge( Soup.from(list));
         }
