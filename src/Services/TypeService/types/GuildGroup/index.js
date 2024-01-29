81fc979349f3bf701ea137732914ca7645b554e1
paige <88659700+paishee@users.noreply.github.com>
2024-01-29 10:40:19 -0600

    Create index.js

---
+++ src/Services/TypeService/types/GuildGroup/index.js
@@ -0,0 +1,21 @@
+const TypeService = require('../../index.js');
+const { Soup } = require('stews');
+const fs = require('fs');
+
+
+TypeService.newC("GuildGroup", class extends Soup {
+    constructor(guild) {
+        super(Object);
+    }
+});
+
+
+module.exports = GuildGroup;
+
+
+let cust_dir = require('./custard/_funkydir');
+let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
+
+cust.forEach( (file) => {
+    require(`./custard/${file}`);
+});
