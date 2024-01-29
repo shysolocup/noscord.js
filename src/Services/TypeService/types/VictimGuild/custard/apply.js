884595cf30de877308dda6db26897875e227cccd
paige <88659700+paishee@users.noreply.github.com>
2024-01-29 08:01:56 -0600

    Create apply.js

---
+++ src/Services/TypeService/types/VictimGuild/custard/apply.js
@@ -0,0 +1,32 @@
+const VictimGuild = require('../index.js');
+
+
+VictimGuild.newF("apply", async function(ctx, actionType=null) {
+    const client = this.parent.parent;
+    client.import("types", "messages", "channels", "guilds", "util");
+
+	let guild = new types.Guild;
+
+	await guild.apply(ctx);
+
+	Object.assign(this, guild);
+	
+	this.created = (actionType == 0);
+    this.deleted = (actionType == 1);
+    this.edited = (actionType == 2);
+
+
+    // times
+	let created = this.created;
+    let deleted = this.deleted;
+    let edited = this.edited;
+    
+    this.timestamps.created = (created) ? new Timestamp() : undefined;
+	this.timestamps.deleted = (deleted) ? new Timestamp() : undefined;
+    this.timestamps.edited = (edited) ? new Timestamp() : undefined;
+
+
+    Object.defineProperty(this, "raw", {
+		get() { return ctx }	
+	});
+});
