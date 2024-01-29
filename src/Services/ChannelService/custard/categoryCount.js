ee9970796f33217078c09b20ed1eb872ca85721c
paige <88659700+paishee@users.noreply.github.com>
2024-01-29 10:28:04 -0600

    Update categoryCount.js

--- src/Services/ChannelService/custard/categoryCount.js
+++ src/Services/ChannelService/custard/categoryCount.js
@@ -3,10 +3,5 @@ const { Soup } = require('stews');
 
 
 ChannelService.newF("categoryCount", async function(guild=null) {
-    return new Soup( await ( (guild) ? ((guild.raw) ? guild.raw : guild) .channels : this.parent._base.channels)
-        .fetch()
-        .catch(e=>{})
-    )
-		.filter( (id, channel) => { return channel.type == 4; })
-		.length;
+    return (await this.categories()).length;
 });
