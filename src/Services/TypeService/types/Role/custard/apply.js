49f965943fbe7b61aa149d1392f9a32aecf549ba
paige <88659700+paishee@users.noreply.github.com>
2024-01-29 11:36:09 -0600

    Update apply.js

--- src/Services/TypeService/types/Role/custard/apply.js
+++ src/Services/TypeService/types/Role/custard/apply.js
@@ -3,13 +3,43 @@ const Role = require('../index.js');
 
 Role.newF("apply", async function(ctx) {
     const client = this.parent.parent;
-    client.import("guilds", "messages", "channels", "users", "util");
+    client.import("guilds", "messages", "channels", "users", "util", "att");
     
     // ids
     this.id = ctx.id;
-    this.mention = `<@&${ctx.id}>`
+    this.mention = `<@&${ctx.id}>`;
+	this.icon = att.create(ctx.iconURL(), util.genCode(9));
+	this.iconUrl = ctx.iconURL();
+	this.iconURL = this.iconUrl;
+	this.position = ctx.position;
+	this.rawPosition = ctx.rawPosition;
+	this.tags = ctx.tags;
+	this.emoji = ctx.unicodeEmoji;
+	this.permissions = ctx.permissions;
+
+	this.color = ctx.color;
+	this.flags = ctx.flags;
+
+	// guild
+	this.guild = await guilds.get(ctx.guild.id);
+	this.guildId = ctx.guild.id;
 
 
+	// booleans
+	this.managed = ctx.managed;
+	this.editable = ctx.editable;
+	this.hoist = ctx.hoist;
+	this.mentionable = ctx.mentionable;
+
+
+	// colors
+    this.color = {
+        base10: ctx.color,
+        hex: ctx.hexColor,
+		hexInt: (ctx.hexColor) ? Number(ctx.hexColor.replace("#", "0x")) : null
+    };
+	
+	
     // times
     this.timestamps = {
         created: new Timestamp(ctx.createdAt),
