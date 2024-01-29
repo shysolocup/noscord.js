bc7e881cc3a9ea9a0616a0a9ad7a2b976ff81e78
paige <88659700+paishee@users.noreply.github.com>
2024-01-29 12:54:13 -0600

    Update index.js

--- src/Services/TypeService/types/PrefixCommandRun/index.js
+++ src/Services/TypeService/types/PrefixCommandRun/index.js
@@ -5,7 +5,7 @@ TypeService.newC("PrefixCommandRun");
 
 module.exports = PrefixCommandRun;
 
-let cust_dir = require('./custard/_funkydir');
+let cust_dir = `${__dirname}/custard`;
 let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
 
 cust.forEach( (file) => {
