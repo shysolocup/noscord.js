ae9b555319b1ef427e3c6c77b73a3b7673f9ef48
paige <88659700+paishee@users.noreply.github.com>
2024-01-29 10:32:59 -0600

    Update list.js

--- src/Services/ChannelService/custard/list.js
+++ src/Services/ChannelService/custard/list.js
@@ -3,10 +3,11 @@ const { Soup } = require('stews');
 
 
 ChannelService.newF("list", async function(guild=null) {
-    return new Soup( await ( (guild) ? ((guild.raw) ? guild.raw : guild) .channels : this.parent._base.channels)
-        .fetch()
-        .catch(e=>{})
-    )
-		.filter( (id, channel) => { return channel.type != 4; })
-		.pour();
+    const client = this.parent;
+    client.import("types");
+    
+   	let group = new types.ChannelGroup;
+	await group.apply(guild);
+
+    return group;
 });
