1e85f779a2c203934baff51f7405c123b6e440d1
paige <88659700+paishee@users.noreply.github.com>
2024-01-29 13:24:23 -0600

    Update index.js

--- src/Services/TypeService/types/VoiceMemberGroup/index.js
+++ src/Services/TypeService/types/VoiceMemberGroup/index.js
@@ -13,7 +13,7 @@ TypeService.newC("VoiceMemberGroup", class extends Soup {
 module.exports = VoiceMemberGroup;
 
 
-let cust_dir = require('./custard/_funkydir');
+let cust_dir = `${__dirname}/custard`;
 let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
 
 cust.forEach( (file) => {
