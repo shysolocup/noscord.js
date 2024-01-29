b6f02f36eed4b3d886a12f921d7914d11d2fbfbb
paige <88659700+paishee@users.noreply.github.com>
2024-01-29 09:58:46 -0600

    Update stickers.js

--- src/Services/ExpressionService/custard/stickers.js
+++ src/Services/ExpressionService/custard/stickers.js
@@ -3,5 +3,16 @@ const { Soup } = require('stews');
 
 
 ExpressionService.newF("stickers", async function(guild) {
-    return Soup.from(await ((guild.raw) ? guild.raw : guild) .stickers.fetch()).pour();
+    const client = this.parent;
+    client.import("types");
+    
+    let stuff = Soup.from(await ((guild.raw) ? guild.raw : guild) .stickers.fetch())
+    for (let [id, raw] of stuff) {
+        let thing = new types.Sticker;
+        await thing.apply(raw);
+
+        stuff.set(id, thing);
+    }
+
+    return stuff;
 });
