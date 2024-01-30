f2e07b530747c790a1cb448f682e8eef98838ede
paige <88659700+paishee@users.noreply.github.com>
2024-01-30 12:55:04 -0600

    Update apply.js

--- src/Services/TypeService/types/GroupChat/custard/apply.js
+++ src/Services/TypeService/types/GroupChat/custard/apply.js
@@ -28,4 +28,29 @@ GroupChat.newF("apply", async function(ctx) {
 		get() { return ctx }	
 	});
 
+	
+	// function forking
+	let pd = Soup.from(Object.getOwnPropertyDescriptors(ctx.__proto__));
+    pd = pd.merge( Soup.from(Object.getOwnPropertyDescriptors(ctx.__proto__.__proto__)) );
+
+    let basepd = Soup.from( Object.getOwnPropertyDescriptors(ctx.__proto__.__proto__.__proto__));
+
+    for ( [ prop, data ] of basepd ) {
+        if (!this.__proto__[prop] && !this[prop]) {
+            if (data.value) data.value = data.value.bind(ctx);
+            else if (data.get) data.get = data.get.bind(ctx);
+
+            Object.defineProperty(this.__proto__, prop, data);
+        }
+    }
+
+    for ( [ prop, data ] of pd ) {
+        if (!this.__proto__[prop] && !this[prop]) {
+            if (data.value && data.value instanceof Function) data.value = data.value.bind(ctx);
+            else if (data.get) data.get = data.get.bind(ctx);
+
+            Object.defineProperty(this.__proto__, prop, data);
+        }
+    }
+	
 });
