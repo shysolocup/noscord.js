27ad8902b8ffecca7379fc88926f5ed1aaca243a
paige <88659700+paishee@users.noreply.github.com>
2024-01-30 08:29:23 -0600

    Delete src/Services/TypeService/types/Groups directory

--- src/Services/TypeService/types/Groups/index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-let type_dir = __dirname
-let types = fs.readdirSync(type_dir).filter( folder => ( folder != "_funkydir" ) );
-
-types.forEach( (folder) => {
-    require(`./${folder}`);
-});
