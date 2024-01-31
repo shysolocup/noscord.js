3a44e80c33914674fe9859018cd6abf2fddf6273
paige <88659700+paishee@users.noreply.github.com>
2024-01-31 09:52:41 -0600

    Create index.js

---
+++ src/Services/TypeService/types/SelectChannelGroup/index.js
@@ -0,0 +1,21 @@
+const TypeService = require('../../index.js');
+const { Soup } = require('stews');
+const fs = require('fs');
+
+
+TypeService.newC("SelectChannelGroup", class extends Soup {
+    constructor() {
+        super(Object);
+    }
+});
+
+
+module.exports = SelectChannelGroup;
+
+
+let cust_dir = `${__dirname}/custard`;
+let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
+
+cust.forEach( (file) => {
+    require(`./custard/${file}`);
+});
