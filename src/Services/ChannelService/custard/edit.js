9a5ea8f00a4adbd172bbb4c6baaec7423743d778
paige <88659700+paishee@users.noreply.github.com>
2024-01-31 08:10:39 -0600

    Create edit.js

---
+++ src/Services/ChannelService/custard/edit.js
@@ -0,0 +1,29 @@
+const ChannelService = require('../index.js');
+
+
+ChannelService.newF("edit", async function(channel, settings={}, reason) {
+    const client = this.parent;
+    client.import("channels");
+
+    if ( (settings.desc || settings.description) && !settings.topic ) {
+        settings.topic = (settings.desc) ? settings.desc : settings.description;
+    }
+
+    if ( (settings.slowmode) && !settings.rateLimitPerUser ) {
+        settings.rateLimitPerUser = settings.slowmode;
+    }
+
+    if ( settings.category || settings.parent ) {
+        let ctg = (settings.category) 
+            ? ((c = settings.category) => ((c.raw) ? c.raw : c) )() 
+            : ((c = settings.parent) => ((c.raw) ? c.raw : c) )() 
+        
+        settings.parent = ctg;
+    }
+
+    let data = (channel.raw) ? channel.raw : channel;
+
+    let edit = this.typer(data.type);
+    await edit.apply( await data.edit(settings).catch(e=>{}) );
+    return edit;
+});
