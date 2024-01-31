1d612d6ee12a5e9a9efa4502c9c75c6b9b43d5e5
paige <88659700+paishee@users.noreply.github.com>
2024-01-31 08:50:41 -0600

    Create index.js

---
+++ src/Services/TypeService/types/SelectMessageGroup/index.js
@@ -0,0 +1,21 @@
+const TypeService = require('../../index.js');
+const { Soup } = require('stews');
+const fs = require('fs');
+
+
+TypeService.newC("SelectMessageGroup", class extends Soup {
+    constructor(guild) {
+        super(Object);
+    }
+});
+
+
+module.exports = SelectMessageGroup;
+
+
+let cust_dir = `${__dirname}/custard`;
+let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
+
+cust.forEach( (file) => {
+    require(`./custard/${file}`);
+});
