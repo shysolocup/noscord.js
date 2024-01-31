1f7a5a85ac1e909ef25596118b52b7312093108d
paige <88659700+paishee@users.noreply.github.com>
2024-01-31 12:41:11 -0600

    Update apply.js

--- src/Services/TypeService/types/VictimMember/custard/apply.js
+++ src/Services/TypeService/types/VictimMember/custard/apply.js
@@ -10,6 +10,7 @@ VictimMember.newF("apply", async function(ctx, actionType=null) {
 	await member.apply(ctx);
 
 	Object.assign(this, member);
+	util.inherits(this.constructor, member);
 	
 	this.joined = (actionType == 0);
 	this.edited = (actionType == 1);
