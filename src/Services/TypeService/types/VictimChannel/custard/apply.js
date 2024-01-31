1778ab868f267569936722fa5ae5fa40900b5933
paige <88659700+paishee@users.noreply.github.com>
2024-01-31 08:27:58 -0600

    Update apply.js

--- src/Services/TypeService/types/VictimChannel/custard/apply.js
+++ src/Services/TypeService/types/VictimChannel/custard/apply.js
@@ -1,4 +1,5 @@
 const VictimChannel = require('../index.js');
+const { Soup } = require('stews');
 
 
 VictimChannel.newF("apply", async function(ctx, actionType=null) {
@@ -6,7 +7,6 @@ VictimChannel.newF("apply", async function(ctx, actionType=null) {
     client.import("types", "messages", "channels", "guilds", "util");
 
 	let channel = channels.typer(ctx.type);
-
 	await channel.apply(ctx);
 
 	Object.assign(this, channel);
@@ -29,4 +29,16 @@ VictimChannel.newF("apply", async function(ctx, actionType=null) {
     Object.defineProperty(this, "raw", {
 		get() { return ctx }	
 	});
+
+	
+	let pd = Soup.from(Object.getOwnPropertyDescriptors(channel.__proto__));
+
+	for ( let [ prop, data ] of pd ) {
+        if (this.__proto__[prop] == undefined && this[prop] == undefined) {
+            if (data.value && data.value instanceof Function) data.value = data.value.bind(channel);
+            else if (data.get) data.get = data.get.bind(channel);
+
+            Object.defineProperty(this.__proto__, prop, data);
+        }
+    }
 });
