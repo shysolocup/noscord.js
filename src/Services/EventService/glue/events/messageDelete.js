2c97dd2da127938a948da8229aa76f5b339b01c1
paige <88659700+paishee@users.noreply.github.com>
2024-02-01 10:02:28 -0600

    Update messageDelete.js

--- src/Services/EventService/glue/events/messageDelete.js
+++ src/Services/EventService/glue/events/messageDelete.js
@@ -6,7 +6,7 @@ module.exports = (handler) => { handler.init(
     
     func = async function (ctx) { // formatting for types and stuff
         let types = this.types;
-        let msg = new types.Message
+        let msg = new types.VictimMessage
         
         await msg.apply(...Array.from(arguments));
         
