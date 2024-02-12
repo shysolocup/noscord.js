4002fa5131106385809faa4cca20b6c61d9a0166
paige <88659700+paishee@users.noreply.github.com>
2024-02-12 13:32:05 -0600

    Create get.js

---
+++ src/Services/StorageService/custard/get.js
@@ -0,0 +1,34 @@
+const Store = require('../Store.js');
+
+
+Store.newF("get", async function(name, messageId) {
+    const client = this.parent.parent;
+    client.import('util');
+
+    let storeName = this.storeName;
+    let guildId = this.guildId;
+
+    const ret = {
+        id: null,
+        data: null,
+        storeName: storeName,
+        guildId: guildId
+    }
+
+    const channel = await this.shandle(name);
+
+    const ctx = (messageId) ? await channel.messages.fetch(messageId) : (await channel.messages.fetch()).first();
+    if (!ctx) return ret;
+
+    let link = ctx.attachments.first().url;
+
+    let data = await fetch(link, { method: "Get" } );
+    let json = await data.json();
+
+    ret.data = json;
+    ret.id = ctx.id;
+
+    // await this.events.get.fire(ret, this);
+
+    return ret;
+})
