0b2cfd5cabe4d55c41e630cdfb58204a6cfbd375
paige <88659700+paishee@users.noreply.github.com>
2024-02-12 13:30:04 -0600

    Update Store.js

--- src/Services/StorageService/Store.js
+++ src/Services/StorageService/Store.js
@@ -1,12 +1,30 @@
 const StorageService = require('./index.js');
+var { ChannelType } = require('discord.js');
+const { Soup } = require('stews');
+const fs = require('fs');
 
 
 StorageService.newC("Store", class {
-    constructor(name, data) {
-        this.parent.stores.push(name, data);
-        return this.parent.stores;
+    constructor(guildId, storeName, shandle) {
+        this.guildId = guildId;
+        this.storeName = storeName;
+        this.shandle = shandle;
+        let stores = this.parent.stores;
+
+        if (!stores.has(guildId)) stores.push(guildId, new Soup(Object));
+        stores.push(storeName, {});
+        
+        return this;
     }
 });
 
 
 module.exports = Store;
+
+
+let cust_dir = `${__dirname}/custard`;
+let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
+
+cust.forEach( (file) => {
+    require(`./custard/${file}`);
+});
