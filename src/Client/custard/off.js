619eeba092812911f2343bab80053db17c4ccf55
paige <88659700+paishee@users.noreply.github.com>
2024-02-13 14:51:02 -0600

    Update off.js

--- src/Client/custard/off.js
+++ src/Client/custard/off.js
@@ -2,11 +2,21 @@ const { Client } = require('../index.js');
 
 
 Client.newF("off", function(event, func) {
-	if (events.includes(event)) {
-    		this.events.get(event).listeners.remove(func);
-	}
+	if (event instanceof Array) event.forEach( ev => {
+		if (events.includes(ev)) {
+    		this.events.get(ev).listeners.remove(func);
+		}
+		else {
+			this._base.off(ev, func);
+		}
+	});
 	else {
-		this._base.off(event, func);
+		if (events.includes(event)) {
+	    		this.events.get(event).listeners.remove(func);
+		}
+		else {
+			this._base.off(event, func);
+		}
 	}
 
 	return func;
