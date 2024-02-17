54c891ac297f4fd94c86d0e1c87ad234678a13e0
paige <88659700+paishee@users.noreply.github.com>
2024-02-17 03:34:56 -0600

    Update get.js

--- src/Services/StorageService/custard/get.js
+++ src/Services/StorageService/custard/get.js
@@ -20,7 +20,8 @@ Store.newF("get", async function(name, messageId) {
     const ctx = (messageId) ? 
         await channel.messages.fetch(messageId) : 
         
-        Object.values(await channel.messages.fetch()).filter( msg => msg.attachments && msg.attachments.length > 0)[0];
+        ( Array.from( (await channel.messages.fetch()) .values() ) )
+        .filter( msg => msg.attachments && msg.attachments.size > 0 )[0];
     
     if (!ctx) return ret;
 
