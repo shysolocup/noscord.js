6cc3ea444a8b7ba067d9508abb571ab1800d7ed2
paige <88659700+paishee@users.noreply.github.com>
2024-02-17 03:35:30 -0600

    Update json.js

--- src/Services/StorageService/custard/json.js
+++ src/Services/StorageService/custard/json.js
@@ -10,7 +10,12 @@ Store.newF("json", async function(name, messageId) {
 
     const channel = await this.shandle(name);
 
-    const ctx = (messageId) ? await channel.messages.fetch(messageId) : (await channel.messages.fetch()).first();
+    const ctx = (messageId) ? 
+        await channel.messages.fetch(messageId) : 
+        
+        ( Array.from( (await channel.messages.fetch()) .values() ) )
+        .filter( msg => msg.attachments && msg.attachments.size > 0 )[0];
+    
     if (!ctx) return ret;
 
     let link = ctx.attachments.first().url;
