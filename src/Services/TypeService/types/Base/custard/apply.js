ede8c008898585c85ff959730b65bf8efb1c4e7f
paige <88659700+paishee@users.noreply.github.com>
2024-02-20 08:29:02 -0600

    Update apply.js

--- src/Services/TypeService/types/Base/custard/apply.js
+++ src/Services/TypeService/types/Base/custard/apply.js
@@ -8,15 +8,28 @@ Base.newF("apply", async function(ctx) {
 
     // id
     this.id = ctx.id;
-	
+
+	// optional info
     if (ctx.name) this.name = ctx.name;
     if (ctx.type) this.type = ctx.type;
+	if (ctx.flags) this.flags = ctx.flags;
+
+
+	// booleans
+	if (ctx.nsfw) this.nsfw = ctx.nsfw;
+	if (ctx.viewable) this.viewable = ctx.viewable;
+	if (ctx.deletable) this.deletable = ctx.deletable;
+	if (ctx.moderatable) this.moderatable = ctx.moderatable;
+	if (ctx.bannable) this.bannable = ctx.bannable;
+	if (ctx.kickable) this.kickable = ctx.kickable;
+	if (ctx.manageable) { this.manageable = ctx.manageable; this.managable = ctx.manageable; }
+
+	
+	// guild info
 	if (ctx.guildId) {
 		this.guild;
     	this.guildId = ctx.guildId;
-	}
 
-    // stuff
-    if (ctx.guildId) this.guild = await guilds.get(ctx.guildId);
-	if (ctx.parentId) this.category = await channels.get(ctx.parentId, this.guild);
+		this.guild = await guilds.get(ctx.guildId);
+	}
 });
