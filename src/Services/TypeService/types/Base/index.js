45cad6c487eb441c2eb924a0317d687d56c35545
paige <88659700+paishee@users.noreply.github.com>
2024-02-20 08:07:08 -0600

    Create index.js

---
+++ src/Services/TypeService/types/Base/index.js
@@ -0,0 +1,13 @@
+const TypeService = require('../../index.js');
+const fs = require('fs');
+
+TypeService.newC("Base");
+
+module.exports = Base;
+
+let cust_dir = `${__dirname}/custard`;
+let cust = fs.readdirSync(cust_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
+
+cust.forEach( (file) => {
+    require(`./custard/${file}`);
+});
