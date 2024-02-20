4a98b79fbf126fafd1ddce49126a80a94d623438
paige <88659700+paishee@users.noreply.github.com>
2024-02-20 08:34:27 -0600

    Update apply.js

--- src/Services/TypeService/types/Base/custard/apply.js
+++ src/Services/TypeService/types/Base/custard/apply.js
@@ -16,6 +16,7 @@ Base.newF("apply", async function(ctx) {
 
 
 	// booleans
+	if (ctx.available) this.available = ctx.available;
 	if (ctx.nsfw) this.nsfw = ctx.nsfw;
 	if (ctx.viewable) this.viewable = ctx.viewable;
 	if (ctx.deletable) this.deletable = ctx.deletable;
@@ -23,7 +24,8 @@ Base.newF("apply", async function(ctx) {
 	if (ctx.bannable) this.bannable = ctx.bannable;
 	if (ctx.kickable) this.kickable = ctx.kickable;
 	if (ctx.manageable) { this.manageable = ctx.manageable; this.managable = ctx.manageable; }
-
+	if (ctx.managed) this.managed = ctx.managed;
+	if (ctx.requiresColons) this.requiresColons = ctx.requiresColons
 	
 	// guild info
 	if (ctx.guildId) {
